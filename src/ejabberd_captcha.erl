HalloAppInc/halloapp-ejabberd-pub

commit c82bb262073eeb142614adb14d5da47c5f6e61f9

Don't validate an option in ejabberd_config:get_option() functions
The commit introduces the following changes:
* Now there is no need to pass validating function in
  ejabberd_config:get_option() functions, because the configuration
  keeps already validated values.
* New function ejabberd_config:get_option/1 is introduced
* Function ejabberd_config:get_option/3 is deprecated. If the function
  is still called, the second argument (validating function) is simply
  ignored.
* The second argument for ejabberd_config:get_option/2 is now
  a default value, not a validating function.

src/ejabberd_captcha.erl
@@ -350,12 +350,7 @@ do_create_image(Key) ->
     end.
 
 get_prog_name() ->
-    case ejabberd_config:get_option(
-           captcha_cmd,
-           fun(FileName) ->
-                   F = iolist_to_binary(FileName),
-                   if F /= <<"">> -> F end
-           end) of
+    case ejabberd_config:get_option(captcha_cmd) of
         undefined ->
             ?DEBUG("The option captcha_cmd is not configured, "
                    "but some module wants to use the CAPTCHA "
@@ -367,10 +362,7 @@ get_prog_name() ->
     end.
 
 get_url(Str) ->
-    CaptchaHost = ejabberd_config:get_option(
-                    captcha_host,
-                    fun iolist_to_binary/1,
-                    <<"">>),
+    CaptchaHost = ejabberd_config:get_option(captcha_host, <<"">>),
     case str:tokens(CaptchaHost, <<":">>) of
       [Host] ->
 	  <<"http://", Host/binary, "/captcha/", Str/binary>>;
@@ -395,8 +387,7 @@ get_transfer_protocol(PortString) ->
     get_captcha_transfer_protocol(PortListeners).
 
 get_port_listeners(PortNumber) ->
-    AllListeners = ejabberd_config:get_option(
-		     listen, fun ejabberd_listener:validate_cfg/1, []),
+    AllListeners = ejabberd_config:get_option(listen, []),
     lists:filter(fun (Listener) when is_list(Listener) ->
 			 case proplists:get_value(port, Listener) of
 			   PortNumber -> true;
@@ -426,9 +417,7 @@ get_captcha_transfer_protocol([_ | Listeners]) ->
 
 is_limited(undefined) -> false;
 is_limited(Limiter) ->
-    case ejabberd_config:get_option(
-           captcha_limit,
-           fun(I) when is_integer(I), I > 0 -> I end) of
+    case ejabberd_config:get_option(captcha_limit) of
       undefined -> false;
       Int ->
 	  case catch gen_server:call(?MODULE,

src/acl.erl
@@ -199,13 +199,13 @@ load_from_config() ->
     lists:foreach(
       fun(Host) ->
               ACLs = ejabberd_config:get_option(
-                       {acl, Host}, fun(V) -> V end, []),
+                       {acl, Host}, []),
               AccessRules = ejabberd_config:get_option(
-                              {access, Host}, fun(V) -> V end, []),
+                              {access, Host}, []),
               AccessRulesNew = ejabberd_config:get_option(
-				 {access_rules, Host}, fun(V) -> V end, []),
+				 {access_rules, Host}, []),
               ShaperRules = ejabberd_config:get_option(
-				 {shaper_rules, Host}, fun(V) -> V end, []),
+				 {shaper_rules, Host}, []),
               lists:foreach(
                 fun({ACLName, SpecList}) ->
                         lists:foreach(
@@ -605,7 +605,7 @@ access_rules_validator(Rules0) ->
 					  (deny) -> true;
 					  (_) -> false
 				       end),
-    throw({replace_with, Rules}).
+    Rules.
 
 
 shaper_rules_validator(Name) when is_atom(Name) ->
@@ -616,7 +616,7 @@ shaper_rules_validator(Rules0) ->
 					  (V2) when is_integer(V2) -> true;
 					  (_) -> false
 				       end),
-    throw({replace_with, Rules}).
+    Rules.
 
 access_shaper_rules_validator([{Type, Acls} = Rule | Rest], RuleTypeCheck) ->
     case RuleTypeCheck(Type) of

src/cyrsasl_digest.erl
@@ -55,7 +55,7 @@
                 check_password :: check_password_fun(),
                 auth_module :: atom(),
                 host = <<"">> :: binary(),
-                hostfqdn = <<"">> :: binary() | [binary()]}).
+                hostfqdn = [] :: [binary()]}).
 
 start(_Opts) ->
     Fqdn = get_local_fqdn(),
@@ -204,8 +204,6 @@ is_digesturi_valid(DigestURICase, JabberDomain,
 	    false
     end.
 
-is_host_fqdn(Host, Fqdn) when is_binary(Fqdn) ->
-    Host == Fqdn;
 is_host_fqdn(_Host, []) ->
     false;
 is_host_fqdn(Host, [Fqdn | _FqdnTail]) when Host == Fqdn ->
@@ -214,26 +212,13 @@ is_host_fqdn(Host, [Fqdn | FqdnTail]) when Host /= Fqdn ->
     is_host_fqdn(Host, FqdnTail).
 
 get_local_fqdn() ->
-    case catch get_local_fqdn2() of
-      Str when is_binary(Str) -> Str;
-      List when is_list(List) -> List;
-      _ ->
-	  <<"unknown-fqdn, please configure fqdn "
-	    "option in ejabberd.yml!">>
-    end.
-
-get_local_fqdn2() ->
-    case ejabberd_config:get_option(
-           fqdn, fun(X) -> X end) of
-        ConfiguredFqdn when is_binary(ConfiguredFqdn) ->
-            ConfiguredFqdn;
-        [A | _] = ConfiguredFqdns when is_binary(A) ->
-            ConfiguredFqdns;
-        undefined ->
-            {ok, Hostname} = inet:gethostname(),
-            {ok, {hostent, Fqdn, _, _, _, _}} =
-            inet:gethostbyname(Hostname),
-            list_to_binary(Fqdn)
+    case ejabberd_config:get_option(fqdn) of
+	undefined ->
+	    {ok, Hostname} = inet:gethostname(),
+	    {ok, {hostent, Fqdn, _, _, _, _}} = inet:gethostbyname(Hostname),
+	    [list_to_binary(Fqdn)];
+	Fqdn ->
+	    Fqdn
     end.
 
 hex(S) ->
@@ -275,5 +260,10 @@ response(KeyVals, User, Passwd, Nonce, AuthzId,
 	  ":", (hex((erlang:md5(A2))))/binary>>,
     hex((erlang:md5(T))).
 
-opt_type(fqdn) -> fun iolist_to_binary/1;
+opt_type(fqdn) ->
+    fun(FQDN) when is_binary(FQDN) ->
+	    [FQDN];
+       (FQDNs) when is_list(FQDNs) ->
+	    [iolist_to_binary(FQDN) || FQDN <- FQDNs]
+    end;
 opt_type(_) -> [fqdn].

src/ejabberd_access_permissions.erl
@@ -239,8 +239,7 @@ get_definitions(#state{definitions = none, fragments_generators = Gens} = State)
 			[{acl,{acl,admin}},
 			 {oauth,[<<"ejabberd:admin">>],[{acl,{acl,admin}}]}],
 			{all, [start, stop]}}}],
-    ApiPerms = ejabberd_config:get_option(api_permissions, fun(A) -> A end,
-					  DefaultOptions),
+    ApiPerms = ejabberd_config:get_option(api_permissions, DefaultOptions),
     AllCommands = ejabberd_commands:get_commands_definition(),
     Frags = lists:foldl(
 	      fun({_Name, Generator}, Acc) ->
@@ -334,7 +333,7 @@ command_matches_patterns(C, [_ | Tail]) ->
 %%%===================================================================
 
 parse_api_permissions(Data) when is_list(Data) ->
-    throw({replace_with, [parse_api_permission(Name, Args) || {Name, Args} <- Data]}).
+    [parse_api_permission(Name, Args) || {Name, Args} <- Data].
 
 parse_api_permission(Name, Args0) ->
     Args = lists:flatten(Args0),
@@ -374,8 +373,6 @@ parse_who(Name, Defs, ParseOauth) when is_list(Defs) ->
 		throw:{invalid_syntax, Msg} ->
 		    report_error(<<"Invalid access rule: '~s' used inside 'who' section for api_permission '~s'">>,
 				 [Msg, Name]);
-		throw:{replace_with, NVal} ->
-		    {access, NVal};
 		error:_ ->
 		    report_error(<<"Invalid access rule '~p' used inside 'who' section for api_permission '~s'">>,
 				 [Val, Name])

src/ejabberd_app.erl
@@ -89,12 +89,7 @@ stop(_State) ->
 %%%
 
 connect_nodes() ->
-    Nodes = ejabberd_config:get_option(
-              cluster_nodes,
-              fun(Ns) ->
-                      true = lists:all(fun is_atom/1, Ns),
-                      Ns
-              end, []),
+    Nodes = ejabberd_config:get_option(cluster_nodes, []),
     lists:foreach(fun(Node) ->
                           net_kernel:connect_node(Node)
                   end, Nodes).
@@ -142,10 +137,7 @@ delete_pid_file() ->
     end.
 
 set_settings_from_config() ->
-    Ticktime = ejabberd_config:get_option(
-                 net_ticktime,
-                 opt_type(net_ticktime),
-                 60),
+    Ticktime = ejabberd_config:get_option(net_ticktime, 60),
     net_kernel:set_net_ticktime(Ticktime).
 
 file_queue_init() ->

src/ejabberd_auth.erl
@@ -498,9 +498,7 @@ backend_type(Mod) ->
     end.
 
 password_format(LServer) ->
-    ejabberd_config:get_option({auth_password_format, LServer},
-			       opt_type(auth_password_format),
-			       plain).
+    ejabberd_config:get_option({auth_password_format, LServer}, plain).
 
 %%%----------------------------------------------------------------------
 %%% Internal functions
@@ -513,8 +511,7 @@ auth_modules() ->
 auth_modules(Server) ->
     LServer = jid:nameprep(Server),
     Default = ejabberd_config:default_db(LServer, ?MODULE),
-    Methods = ejabberd_config:get_option(
-                {auth_method, LServer}, opt_type(auth_method), [Default]),
+    Methods = ejabberd_config:get_option({auth_method, LServer}, [Default]),
     [misc:binary_to_atom(<<"ejabberd_auth_",
 			   (misc:atom_to_binary(M))/binary>>)
      || M <- Methods].

src/ejabberd_auth_anonymous.erl
@@ -26,9 +26,11 @@
 -module(ejabberd_auth_anonymous).
 
 -behaviour(ejabberd_config).
+-behaviour(ejabberd_auth).
 -author('mickael.remond@process-one.net').
 
 -export([start/1,
+	 stop/1,
 	 allow_anonymous/1,
 	 is_sasl_anonymous_enabled/1,
 	 is_login_anonymous_enabled/1,
@@ -58,6 +60,12 @@ start(Host) ->
     ejabberd_hooks:add(sm_remove_connection_hook, Host,
 		       ?MODULE, unregister_connection, 100),
     ok.
+
+stop(Host) ->
+    ejabberd_hooks:delete(sm_register_connection_hook, Host,
+			  ?MODULE, register_connection, 100),
+    ejabberd_hooks:delete(sm_remove_connection_hook, Host,
+			  ?MODULE, unregister_connection, 100).
 
 %% Return true if anonymous is allowed for host or false otherwise
 allow_anonymous(Host) ->
@@ -93,21 +101,12 @@ is_login_anonymous_enabled(Host) ->
 %% Return the anonymous protocol to use: sasl_anon|login_anon|both
 %% defaults to login_anon
 anonymous_protocol(Host) ->
-    ejabberd_config:get_option(
-      {anonymous_protocol, Host},
-      fun(sasl_anon) -> sasl_anon;
-         (login_anon) -> login_anon;
-         (both) -> both
-      end,
-      sasl_anon).
+    ejabberd_config:get_option({anonymous_protocol, Host}, sasl_anon).
 
 %% Return true if multiple connections have been allowed in the config file
 %% defaults to false
 allow_multiple_connections(Host) ->
-    ejabberd_config:get_option(
-      {allow_multiple_connections, Host},
-      fun(V) when is_boolean(V) -> V end,
-      false).
+    ejabberd_config:get_option({allow_multiple_connections, Host}, false).
 
 anonymous_user_exist(User, Server) ->
     lists:any(

src/ejabberd_auth_external.erl
@@ -48,12 +48,7 @@
 %%% API
 %%%----------------------------------------------------------------------
 start(Host) ->
-    Cmd = ejabberd_config:get_option(
-            {extauth_program, Host},
-            fun(V) ->
-                    binary_to_list(iolist_to_binary(V))
-            end,
-            "extauth"),
+    Cmd = ejabberd_config:get_option({extauth_program, Host}, "extauth"),
     extauth:start(Host, Cmd),
     check_cache_last_options(Host),
     ejabberd_auth_mnesia:start(Host).
@@ -179,12 +174,8 @@ remove_user(User, Server, Password) ->
 
 %% @spec (Host::string()) -> false | {true, CacheTime::integer()}
 get_cache_option(Host) ->
-    case ejabberd_config:get_option(
-           {extauth_cache, Host},
-           fun(false) -> undefined;
-              (I) when is_integer(I), I >= 0 -> I
-           end) of
-        undefined -> false;
+    case ejabberd_config:get_option({extauth_cache, Host}, false) of
+        false -> false;
         CacheTime -> {true, CacheTime}
     end.
 
@@ -319,12 +310,11 @@ get_mod_last_configured(Server) ->
     end.
 
 is_configured(Host, Module) ->
-    Os = ejabberd_config:get_option({modules, Host},
-					  fun(M) when is_list(M) -> M end),
+    Os = ejabberd_config:get_option({modules, Host}, []),
     lists:keymember(Module, 1, Os).
 
 opt_type(extauth_cache) ->
-    fun (false) -> undefined;
+    fun (false) -> false;
 	(I) when is_integer(I), I >= 0 -> I
     end;
 opt_type(extauth_program) ->

commit 58724f47a17bfcb786ec44ee547ef9dd47cced4f

Fix regression introduced by c82bb262073eeb1
Fixes #1928

src/ejabberd_captcha.erl
@@ -388,29 +388,24 @@ get_transfer_protocol(PortString) ->
 
 get_port_listeners(PortNumber) ->
     AllListeners = ejabberd_config:get_option(listen, []),
-    lists:filter(fun (Listener) when is_list(Listener) ->
-			 case proplists:get_value(port, Listener) of
-			   PortNumber -> true;
-			   _ -> false
-			 end;
-		     (_) -> false
-		 end,
-		 AllListeners).
+    lists:filter(
+      fun({{Port, _IP, _Transport}, _Module, _Opts}) ->
+	      Port == PortNumber
+      end, AllListeners).
 
 get_captcha_transfer_protocol([]) ->
     throw(<<"The port number mentioned in captcha_host "
 	    "is not a ejabberd_http listener with "
 	    "'captcha' option. Change the port number "
 	    "or specify http:// in that option.">>);
-get_captcha_transfer_protocol([Listener | Listeners]) when is_list(Listener) ->
-    case proplists:get_value(module, Listener) == ejabberd_http andalso
-	   proplists:get_bool(captcha, Listener) of
+get_captcha_transfer_protocol([{_, ejabberd_http, Opts} | Listeners]) ->
+    case proplists:get_bool(captcha, Opts) of
       true ->
-	  case proplists:get_bool(tls, Listener) of
-	    true -> https;
-	    false -> http
-	  end;
-      false -> get_captcha_transfer_protocol(Listeners)
+	    case proplists:get_bool(tls, Opts) of
+		true -> https;
+		false -> http
+	    end;
+	false -> get_captcha_transfer_protocol(Listeners)
     end;
 get_captcha_transfer_protocol([_ | Listeners]) ->
     get_captcha_transfer_protocol(Listeners).