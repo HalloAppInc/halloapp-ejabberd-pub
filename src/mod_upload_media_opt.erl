30cdca1b93330095c9cced1c52977468247d79db
Vipin Gupta <vipin@halloapp.com>
2020-02-07 22:13:04 -0800

    Fetch AWS creds from the instance metadata
    erlcloud library fetches IAM creds from ec2 instance metadata if
    possible and refreshes them 5 minutes before expiry.

--- src/mod_upload_media_opt.erl
+++ src/mod_upload_media_opt.erl
@@ -3,25 +3,11 @@
 
 -module(mod_upload_media_opt).
 
--export([aws_access_key/1]).
 -export([aws_media_bucket/1]).
--export([aws_secret_access_key/1]).
-
--spec aws_access_key(gen_mod:opts() | global | binary()) -> 'undefined' | binary().
-aws_access_key(Opts) when is_map(Opts) ->
-    gen_mod:get_opt(aws_access_key, Opts);
-aws_access_key(Host) ->
-    gen_mod:get_module_opt(Host, mod_upload_media, aws_access_key).
 
 -spec aws_media_bucket(gen_mod:opts() | global | binary()) -> 'undefined' | binary().
 aws_media_bucket(Opts) when is_map(Opts) ->
     gen_mod:get_opt(aws_media_bucket, Opts);
 aws_media_bucket(Host) ->
     gen_mod:get_module_opt(Host, mod_upload_media, aws_media_bucket).
-
--spec aws_secret_access_key(gen_mod:opts() | global | binary()) -> 'undefined' | binary().
-aws_secret_access_key(Opts) when is_map(Opts) ->
-    gen_mod:get_opt(aws_secret_access_key, Opts);
-aws_secret_access_key(Host) ->
-    gen_mod:get_module_opt(Host, mod_upload_media, aws_secret_access_key).
 
