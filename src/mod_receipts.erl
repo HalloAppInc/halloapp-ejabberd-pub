d09376674a10702649f2d2ead7b2a183fdcb1d80
murali <murali@halloapp.com>
2020-02-15 00:09:28 -0800

    Adding mod_receipts to update timestamp on receipt messages
    - Clients will soon be sending the receipt messages containing the xml elements from xep0184 to indicate delivery/seen receipts.
    - Server will then receive these packets, update the timestamp accordingly and send it to the client.
    - The client will be using the timestamp set by the server as the time acknowledged by the client for delivery/seen receipt.
    - Also, updated the xml_compress.erl module with new rules to compress these message stanzas
      to include the id and timestamp for both received and seen xml elements.

---
+++ src/mod_receipts.erl
@@ -0,0 +1,94 @@
+%%%----------------------------------------------------------------------
+%%% File    : mod_receipts.erl
+%%%
+%%% Copyright (C) 2019 halloappinc.
+%%%
+%%% This module registers using the user_send_packet hook to be notified 
+%%% on every new packet received by the server. If the packet is a message stanza
+%%% and if it is about a receipt of the message by a client to another client,
+%%% the server updates the timestamp on the receipt and returns the new packet.
+%%% This is useful so that the clients need not worry about having the wrong
+%%% timestamp on the devices.
+%%% Currently, we do this for both delivery and seen receipts.
+%%%----------------------------------------------------------------------
+
+-module(mod_receipts).
+
+-behaviour(gen_mod).
+
+-include("logger.hrl").
+-include("xmpp.hrl").
+
+%% gen_mod API.
+-export([start/2, stop/1, depends/2, mod_options/1, reload/3]).
+%% Hooks.
+-export([user_send_packet/1]).
+
+-type state() :: ejabberd_c2s:state().
+
+start(Host, _Opts) ->
+    ejabberd_hooks:add(user_send_packet, Host, ?MODULE, user_send_packet, 10),
+    ok.
+
+stop(Host) ->
+    ejabberd_hooks:delete(user_send_packet, Host, ?MODULE, user_send_packet, 10),
+    ok.
+
+depends(_Host, _Opts) ->
+    [].
+
+mod_options(_Host) ->
+    [].
+
+reload(_Host, _NewOpts, _OldOpts) ->
+    ok.
+
+
+%% This hook is invoked on every packet received from the user.
+%% We check if the packet has a receipt element (could be delivery/seen) and add a timestamp.
+-spec user_send_packet({stanza(), state()}) -> {stanza(), state()}.
+user_send_packet({Packet, State}) ->
+	TimestampSec = util:convert_timestamp_to_binary(erlang:timestamp()),
+	NewPacket = update_timestamp_if_receipts_message(Packet, TimestampSec),
+	{NewPacket, State}.
+
+
+
+%% Update timestamp if the packet is message with a receipt subelement within the stanza.
+%% Currently, we handle both delivery and seen receipts.
+-spec update_timestamp_if_receipts_message(stanza(), binary()) -> stanza().
+update_timestamp_if_receipts_message(
+				#message{sub_els = [#xmlel{name = <<"seen">>} = SeenXmlEl]} = Packet,
+																					TimestampSec) ->
+	SeenXmlElement = xmpp:decode(SeenXmlEl),
+	T = SeenXmlElement#receipt_seen.timestamp,
+	case T of
+		<<>> ->
+			NewPacket = xmpp:set_els(Packet,
+										[SeenXmlElement#receipt_seen{timestamp = TimestampSec}]);
+		_ ->
+			NewPacket = Packet
+	end,
+	?DEBUG("mod_receipts: user_send_packet: updated the timestamp on this packet: ~p", [NewPacket]),
+	NewPacket;
+
+update_timestamp_if_receipts_message(
+				#message{sub_els = [#xmlel{name = <<"received">>} = ReceivedXmlEl]} = Packet,
+																					TimestampSec) ->
+	ReceivedXmlElement = xmpp:decode(ReceivedXmlEl),
+	T = ReceivedXmlElement#receipt_response.timestamp,
+	case T of
+		<<>> ->
+			NewPacket = xmpp:set_els(Packet,
+								[ReceivedXmlElement#receipt_response{timestamp = TimestampSec}]);
+		_ ->
+			NewPacket = Packet
+	end,
+	?DEBUG("mod_receipts: user_send_packet: updated the timestamp on this packet: ~p", [NewPacket]),
+	NewPacket;
+
+update_timestamp_if_receipts_message(Packet, _TimestampSec) ->
+	?DEBUG("mod_receipts: user_send_packet: this packet: ~p is not modified at all.", [Packet]),
+	Packet.
+
+
