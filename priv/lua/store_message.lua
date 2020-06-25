cb05b8b3648f0e75d37aa59612734bfd8cf8e633
Ethan Rosenthal <ethan@halloapp.com>
2020-06-25 17:46:07 -0400

    use macro as key instead

--- priv/lua/store_message.lua
+++ priv/lua/store_message.lua
@@ -1,1 +1,7 @@
-local OrderId = redis.call('INCR', KEYS[1]) redis.call('HSET', KEYS[2], 'tuid', ARGV[1], 'm', ARGV[2], 'ct', ARGV[3], 'rc', 0, 'ord', OrderId) if ARGV[4] == 'undefined' then else redis.call('HSET', KEYS[2], 'fuid', ARGV[4]) end redis.call('ZADD', KEYS[3], OrderId, ARGV[5])
+local OrderId = redis.call('INCR', KEYS[1])
+redis.call('HSET', KEYS[2], 'tuid', ARGV[1], 'm', ARGV[2], 'ct', ARGV[3], 'rc', 0, 'ord', OrderId)
+if ARGV[4] == 'undefined' then
+else
+    redis.call('HSET', KEYS[2], 'fuid', ARGV[4])
+end
+redis.call('ZADD', KEYS[3], OrderId, ARGV[5])
