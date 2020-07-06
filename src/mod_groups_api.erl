58c63da7943dcad18305fd41b511ba6bb4a54ea5
Nikola Borisov <nikola@halloapp.com>
2020-07-06 14:04:09 -0700

    Groups API implementation (#316)
    * Adding IQ handling for groups.
    
    Handle the cleint requests and translate to the
    mod_groups API.
    
    * Cleanup the diff.
    
    Remove some unused code.
    Fixing new lines.
    Refactor the code.
    Add process function
    Fix empty lines.
    Change number of empty lines.

--- src/mod_groups_api.erl
+++ src/mod_groups_api.erl
@@ -11,7 +11,7 @@
 -behaviour(gen_mod).
 
 %% gen_mod api
--export([start/2, stop/1, reload/3, mod_options/1, depends/2]).
+-export([start/2, stop/1, mod_options/1, depends/2]).
 
 -export([
     process_local_iq/1
@@ -21,6 +21,8 @@
 -include("xmpp.hrl").
 -include("groups.hrl").
 
+-define(NS_GROUPS, <<"halloapp:groups">>).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%   gen_mod API                                                                              %%%
@@ -29,17 +31,13 @@
 
 start(Host, _Opts) ->
     ?INFO_MSG("start", []),
-    gen_iq_handler:add_iq_handler(ejabberd_local, Host, ?NS_TIME, ?MODULE, process_local_iq),
+    gen_iq_handler:add_iq_handler(ejabberd_local, Host, ?NS_GROUPS, ?MODULE, process_local_iq),
     ok.
 
 
 stop(Host) ->
     ?INFO_MSG("stop", []),
-    gen_iq_handler:remove_iq_handler(ejabberd_local, Host, ?NS_TIME),
-    ok.
-
-
-reload(_Host, _NewOpts, _OldOpts) ->
+    gen_iq_handler:remove_iq_handler(ejabberd_local, Host, ?NS_GROUPS),
     ok.
 
 
@@ -50,13 +48,57 @@ depends(_Host, _Opts) ->
 mod_options(_Host) ->
     [].
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%   API                                                                                      %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%% create_group %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = set,
+        sub_els = [#group_st{action = create, name = Name} = ReqGroupSt]} = IQ) ->
+    process_create_group(IQ, Uid, Name, ReqGroupSt);
 
-process_local_iq(#iq{} = _IQ) ->
-    ok.
+
+%%% delete_group %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = set,
+        sub_els = [#group_st{action = delete, gid = Gid} = _ReqGroupSt]} = IQ) ->
+    process_delete_group(IQ, Gid, Uid);
+
+
+%%% modify_members %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = set,
+        sub_els = [#group_st{action = modify_members, gid = Gid} = ReqGroupSt]} = IQ) ->
+    process_modify_members(IQ, Gid, Uid, ReqGroupSt);
+
+
+%%% modify_admins %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = set,
+        sub_els = [#group_st{action = modify_admins, gid = Gid} = ReqGroupSt]} = IQ) ->
+    process_modify_admins(IQ, Gid, Uid, ReqGroupSt);
+
+
+%%% get_group %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = get,
+        sub_els = [#group_st{action = get, gid = Gid} = _ReqGroupSt]} = IQ) ->
+    process_get_group(IQ, Gid, Uid);
+
+
+%%% get_groups %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = get,
+        sub_els = [#groups{action = get}]} = IQ) ->
+    process_get_groups(IQ, Uid);
+
+
+%%% set_name %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = set,
+        sub_els = [#group_st{action = set_name, gid = Gid, name = Name} = _ReqGroupSt]} = IQ) ->
+    process_set_name(IQ, Gid, Uid, Name);
+
+
+%%% leave_group %%%
+process_local_iq(#iq{from = #jid{luser = Uid}, type = set,
+        sub_els = [#group_st{action = leave, gid = Gid} = _ReqGroupSt]} = IQ) ->
+    process_leave_group(IQ, Gid, Uid).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -64,17 +106,188 @@ process_local_iq(#iq{} = _IQ) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%-spec process_local_iq(iq()) -> iq().
-%%process_local_iq(#iq{type = set, lang = Lang} = IQ) ->
-%%    Txt = ?T("Value 'set' of 'type' attribute is not allowed"),
-%%    xmpp:make_error(IQ, xmpp:err_not_allowed(Txt, Lang));
-%%process_local_iq(#iq{type = get} = IQ) ->
-%%    Now = erlang:timestamp(),
-%%    Now_universal = calendar:now_to_universal_time(Now),
-%%    Now_local = calendar:universal_time_to_local_time(Now_universal),
-%%    Seconds_diff =
-%%	calendar:datetime_to_gregorian_seconds(Now_local) -
-%%	calendar:datetime_to_gregorian_seconds(Now_universal),
-%%    {Hd, Md, _} = calendar:seconds_to_time(abs(Seconds_diff)),
-%%    xmpp:make_iq_result(IQ, #time{tzo = {Hd, Md}, utc = Now}).
+-spec process_create_group(IQ :: iq(), Uid :: uid(),
+        Name :: binary(), ReqGroupSt :: group_st()) -> iq().
+process_create_group(IQ, Uid, Name, ReqGroupSt) ->
+    ?INFO_MSG("create_group Uid: ~s Name: |~s| Group: ~p", [Uid, Name, ReqGroupSt]),
+    MemberUids = [M#member_st.uid || M <- ReqGroupSt#group_st.members],
+
+    {ok, Group, Results} = mod_groups:create_group(Uid, Name, MemberUids),
+
+    MembersSt = lists:map(
+        fun ({MemberUid, add, Result}) ->
+            Type = case MemberUid =:= Uid of
+                true -> admin;
+                false -> member
+            end,
+            make_member_st(MemberUid, Result, Type)
+        end,
+        Results),
+
+    GroupStResult = #group_st{
+        gid = Group#group.gid,
+        name = Group#group.name,
+        action = create,
+        members = MembersSt
+    },
+    xmpp:make_iq_result(IQ, GroupStResult).
+
+
+-spec process_delete_group(IQ :: iq(), Gid :: gid(), Uid :: uid()) -> iq().
+process_delete_group(IQ, Gid, Uid) ->
+    ?INFO_MSG("delete_group Gid: ~s Uid: ~s", [Gid, Uid]),
+    %% TODO: implement
+    ?ERROR_MSG("delete_group unimplemented", []),
+    xmpp:make_error(IQ, xmpp:err_feature_not_implemented()).
+
+
+-spec process_modify_members(IQ :: iq(), Gid :: gid(), Uid :: uid(), ReqGroupSt :: group_st())
+            -> iq().
+process_modify_members(IQ, Gid, Uid, ReqGroupSt) ->
+    MembersSt = ReqGroupSt#group_st.members,
+    Changes = [{M#member_st.uid, M#member_st.action} || M <- MembersSt],
+    ?INFO_MSG("modify_members Gid: ~s Uid: ~s Changes: ~p", [Gid, Uid, Changes]),
+    case mod_groups:modify_members(Gid, Uid, Changes) of
+        {error, not_admin} ->
+            xmpp:make_error(IQ, err(not_admin));
+        {ok, ModifyResults} ->
+
+            ResultMemberSt = lists:map(
+                fun ({Ouid, Action, Result}) ->
+                    make_member_st(Ouid, Result, member, Action)
+                end,
+                ModifyResults),
+
+            GroupStResult = #group_st{
+                gid = Gid,
+                action = modify_members,
+                members = ResultMemberSt
+            },
+            xmpp:make_iq_result(IQ, GroupStResult)
+    end.
+
+
+-spec process_modify_admins(IQ :: iq(), Gid :: gid(), Uid :: uid(), ReqGroupSt :: group_st())
+            -> iq().
+process_modify_admins(IQ, Gid, Uid, ReqGroupSt) ->
+    MembersSt = ReqGroupSt#group_st.members,
+    Changes = [{M#member_st.uid, M#member_st.action} || M <- MembersSt],
+    ?INFO_MSG("modify_admins Gid: ~s Uid: ~s Changes: ~p", [Gid, Uid, Changes]),
+
+    case mod_groups:modify_admins(Gid, Uid, Changes) of
+        {error, not_admin} ->
+            xmpp:make_error(IQ, err(not_admin));
+        {ok, ModifyResults} ->
+
+            ResultMemberSt = lists:map(
+                fun ({Ouid, Action, Result}) ->
+                    Type = case Action of promote -> admin; demote -> member end,
+                    make_member_st(Ouid, Result, Type, Action)
+                end,
+                ModifyResults),
+
+            GroupStResult = #group_st{
+                gid = Gid,
+                action = modify_admins,
+                members = ResultMemberSt
+            },
+            xmpp:make_iq_result(IQ, GroupStResult)
+    end.
+
+
+-spec process_get_group(IQ :: iq(), Gid :: gid(), Uid :: uid()) -> iq().
+process_get_group(IQ, Gid, Uid) ->
+    ?INFO_MSG("get_group Gid: ~s Uid: ~s", [Gid, Uid]),
+    case mod_groups:get_group(Gid, Uid) of
+        {error, not_member} ->
+            xmpp:make_error(IQ, err(not_member));
+        {ok, Group} ->
+            GroupSt = make_group_st(Group),
+            xmpp:make_iq_result(IQ, GroupSt)
+    end.
+
+
+-spec process_get_groups(IQ :: iq(), Uid :: uid()) -> iq().
+process_get_groups(IQ, Uid) ->
+    ?INFO_MSG("get_groups Uid: ~s", [Uid]),
+    GroupInfos = mod_groups:get_groups(Uid),
+    GroupsSt = [group_info_to_group_st(GI) || GI <- GroupInfos],
+    ResultSt = #groups{
+        action = get,
+        groups = GroupsSt
+    },
+    xmpp:make_iq_result(IQ, ResultSt).
+
+
+-spec process_set_name(IQ :: iq(), Gid :: gid(), Uid :: uid(), Name :: name()) -> iq().
+process_set_name(IQ, Gid, Uid, Name) ->
+    ?INFO_MSG("set_name Gid: ~s Uid: ~s Name: |~p|", [Gid, Uid, Name]),
+    case mod_groups:set_name(Gid, Uid, Name) of
+        {error, invalid_name} ->
+            xmpp:make_error(IQ, err(invalid_name));
+        {error, not_member} ->
+            xmpp:make_error(IQ, err(not_member));
+        ok ->
+            {ok, GroupInfo} = mod_groups:get_group_info(Gid, Uid),
+            xmpp:make_iq_result(IQ, group_info_to_group_st(GroupInfo))
+    end.
+
+
+-spec process_leave_group(IQ :: iq(), Gid :: gid(), Uid :: uid()) -> iq().
+process_leave_group(IQ, Gid, Uid) ->
+    ?INFO_MSG("leave_group Gid: ~s Uid: ~s ", [Gid, Uid]),
+    case mod_groups:leave_group(Gid, Uid) of
+        {ok, _Res} ->
+            xmpp:make_iq_result(IQ)
+    end.
+
+
+-spec group_info_to_group_st(GroupInfo :: group_info()) -> group_st().
+group_info_to_group_st(GroupInfo) ->
+    #group_st{
+        gid = GroupInfo#group_info.gid,
+        name = GroupInfo#group_info.name,
+        avatar = GroupInfo#group_info.avatar
+    }.
+
+
+-spec make_group_st(Group :: group()) -> group_st().
+make_group_st(Group) ->
+    #group_st{
+        gid = Group#group.gid,
+        name = Group#group.name,
+        avatar = Group#group.avatar,
+        members = make_members_st(Group#group.members)
+    }.
+
+
+-spec make_members_st(Members :: [group_member()]) -> [member_st()].
+make_members_st(Members) ->
+    [#member_st{
+        uid = M#group_member.uid,
+        type = M#group_member.type
+    } || M <- Members].
+
+
+make_member_st(MemberUid, Result, Type, Action) ->
+    S = make_member_st(MemberUid, Result, Type),
+    S#member_st{action = Action}.
+
+
+make_member_st(MemberUid, Result, Type) ->
+    {Result2, Reason} = case Result of
+        ok -> {ok, <<>>};
+        Result -> {failed, Result}
+    end,
+    #member_st{
+        uid = MemberUid,
+        result = Result2,
+        reason = Reason,
+        type = Type
+    }.
+
+
+-spec err(Reason :: atom()) -> stanza_error().
+err(Reason) ->
+    #stanza_error{reason = Reason}.
 
---
+++ test/mod_groups_api_tests.erl
@@ -0,0 +1,395 @@
+%%%-------------------------------------------------------------------
+%%% File: model_phone_tests.erl
+%%% Copyright (C) 2020, Halloapp Inc.
+%%%
+%%%-------------------------------------------------------------------
+-module(mod_groups_api_tests).
+-author("nikola").
+
+-include("xmpp.hrl").
+-include("groups.hrl").
+
+-include_lib("eunit/include/eunit.hrl").
+
+% TODO: move this data into header file to share across the group tests
+-define(GROUP_NAME1, <<"Test Group 1">>).
+-define(GROUP_NAME2, <<"Test Group 2">>).
+-define(GROUP_NAME3, <<"Test Group 3">>).
+-define(UID1, <<"1">>).
+-define(UID2, <<"2">>).
+-define(UID3, <<"3">>).
+-define(UID4, <<"4">>).
+-define(UID5, <<"5">>).
+-define(PHONE1, <<"12065550001">>).
+-define(PHONE2, <<"12065550002">>).
+-define(PHONE3, <<"12065550003">>).
+-define(PHONE4, <<"12065550004">>).
+-define(NAME1, <<"Name1">>).
+-define(NAME2, <<"Name2">>).
+-define(NAME3, <<"Name3">>).
+-define(NAME4, <<"Name4">>).
+-define(UA, <<"HalloApp/Android1.0">>).
+
+
+setup() ->
+    gen_iq_handler:start(ejabberd_local),
+    mod_redis:start(undefined, []),
+    clear(),
+    setup_accounts(),
+    ok.
+
+
+setup_accounts() ->
+    model_accounts:create_account(?UID1, ?PHONE1, ?NAME1, ?UA),
+    model_accounts:create_account(?UID2, ?PHONE2, ?NAME2, ?UA),
+    model_accounts:create_account(?UID3, ?PHONE3, ?NAME3, ?UA),
+    model_accounts:create_account(?UID4, ?PHONE4, ?NAME4, ?UA),
+    ok.
+
+
+clear() ->
+    ok = gen_server:cast(redis_groups_client, flushdb),
+    ok = gen_server:cast(redis_accounts_client, flushdb).
+
+
+create_group_IQ(Uid, Name) ->
+    create_group_IQ(Uid, Name, []).
+
+
+create_group_IQ(Uid, Name, Uids) ->
+    MemberSt = [#member_st{uid = Ouid} || Ouid <- Uids],
+    #iq{
+        from = #jid{luser = Uid},
+        type = set,
+        sub_els = [
+            #group_st{
+                action = create,
+                name = Name,
+                members = MemberSt
+            }
+        ]
+    }.
+
+
+delete_group_IQ(Uid, Gid) ->
+    make_group_IQ(Uid, Gid, set, delete, []).
+
+
+modify_members_IQ(Uid, Gid, Changes) ->
+    make_group_IQ(Uid, Gid, set, modify_members, Changes).
+
+
+modify_admins_IQ(Uid, Gid, Changes) ->
+    make_group_IQ(Uid, Gid, set, modify_admins, Changes).
+
+
+get_group_IQ(Uid, Gid) ->
+    make_group_IQ(Uid, Gid, get, get, []).
+
+
+leave_group_IQ(Uid, Gid) ->
+    make_group_IQ(Uid, Gid, set, leave, []).
+
+
+set_name_IQ(Uid, Gid, Name) ->
+    #iq{
+        from = #jid{luser = Uid},
+        type = set,
+        sub_els = [
+            #group_st{
+                gid = Gid,
+                action = set_name,
+                name = Name
+            }
+        ]
+    }.
+
+
+make_group_IQ(Uid, Gid, Type, Action, Changes) ->
+    MemberSt = [#member_st{uid = Ouid, action = MAction} || {Ouid, MAction} <- Changes],
+    #iq{
+        from = #jid{luser = Uid},
+        type = Type,
+        sub_els = [
+            #group_st{
+                gid = Gid,
+                action = Action,
+                members = MemberSt
+            }
+        ]
+    }.
+
+
+get_groups_IQ(Uid) ->
+    #iq{
+        from = #jid{luser = Uid},
+        type = get,
+        sub_els = [
+            #groups{action = get}
+        ]
+    }.
+
+
+get_result_iq_sub_el(#iq{} = IQ) ->
+    ?assertEqual(result, IQ#iq.type),
+    [Res] = IQ#iq.sub_els,
+    Res.
+
+
+get_error_iq_sub_el(#iq{} = IQ) ->
+    ?assertEqual(error, IQ#iq.type),
+    [Res] = IQ#iq.sub_els,
+    Res.
+
+
+mod_groups_api_test() ->
+    setup(),
+    Host = <<"s.halloapp.net">>,
+    ?assertEqual(ok, mod_groups_api:start(Host, [])),
+    ?assertEqual(ok, mod_groups_api:stop(Host)),
+    ?assertEqual([{mod_groups, hard}], mod_groups_api:depends(Host, [])),
+    ?assertEqual([], mod_groups_api:mod_options(Host)),
+    ok.
+
+
+create_empty_group_test() ->
+    setup(),
+    IQ = create_group_IQ(?UID1, ?GROUP_NAME1),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+    #group_st{
+        gid = Gid,
+        name = ?GROUP_NAME1,
+        members = []
+    } = GroupSt,
+    ?assertEqual(true, model_groups:group_exists(Gid)),
+    ?assertEqual(
+        {ok, #group_info{gid = Gid, name = ?GROUP_NAME1}},
+        mod_groups:get_group_info(Gid, ?UID1)),
+%%    ?debugVal(IQRes),
+    ok.
+
+
+create_group_with_members_test() ->
+    setup(),
+    IQ = create_group_IQ(?UID1, ?GROUP_NAME1, [?UID2, ?UID3, ?UID5]),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+    #group_st{
+        gid = _Gid,
+        name = ?GROUP_NAME1,
+        members = Members
+    } = GroupSt,
+%%    ?debugVal(Members),
+    M2 = #member_st{uid = ?UID2, type = member, result = ok},
+    M3 = #member_st{uid = ?UID3, type = member, result = ok},
+    M5 = #member_st{uid = ?UID5, type = member, result = failed, reason = no_account},
+    ?assertEqual([M2, M3, M5], Members),
+    ok.
+
+
+create_group_with_creator_as_member_test() ->
+    setup(),
+    IQ = create_group_IQ(?UID1, ?GROUP_NAME1, [?UID1, ?UID2]),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+    #group_st{
+        gid = _Gid,
+        name = ?GROUP_NAME1,
+        members = Members
+    } = GroupSt,
+%%    ?debugVal(Members),
+    M1 = #member_st{uid = ?UID1, type = admin, result = ok},
+    M2 = #member_st{uid = ?UID2, type = member, result = ok},
+    ?assertEqual([M1, M2], Members),
+    ok.
+
+
+delete_group_test() ->
+    setup(),
+    Gid1 = undefined,
+    IQ = delete_group_IQ(?UID1, Gid1),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    ExpectedError = xmpp:err_feature_not_implemented(),
+%%    ?debugVal(IQRes),
+    ?assertMatch(
+        #iq{
+            type = error,
+            sub_els = [ExpectedError]
+        },
+        IQRes),
+    ok.
+
+
+create_group(Uid, Name, Members) ->
+    IQ = create_group_IQ(Uid, Name, Members),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+    #group_st{gid = Gid} = GroupSt,
+    Gid.
+
+
+modify_members_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+%%    ?debugVal(Gid, 1000),
+    IQ = modify_members_IQ(?UID1, Gid, [{?UID2, remove}, {?UID4, add}, {?UID5, add}]),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+%%    ?debugVal(GroupSt, 1000),
+    ?assertMatch(
+        #group_st{
+            gid = Gid,
+            action = modify_members,
+            members = [
+                #member_st{uid = ?UID2, action = remove, result = ok},
+                #member_st{uid = ?UID4, action = add, type = member, result = ok},
+                #member_st{uid = ?UID5, action = add, type = member,
+                    result = failed, reason = no_account}]
+        },
+        GroupSt),
+    ok.
+
+
+modify_members_not_admin_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+%%    ?debugVal(Gid, 1000),
+    IQ = modify_members_IQ(?UID2, Gid, [{?UID4, add}, {?UID5, add}]),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    Error = get_error_iq_sub_el(IQRes),
+%%    ?debugVal(Error, 1000),
+    ?assertEqual(#stanza_error{reason = not_admin}, Error),
+    ok.
+
+
+modify_admins_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+%%    ?debugVal(Gid, 1000),
+    IQ = modify_admins_IQ(?UID1, Gid, [{?UID2, promote}, {?UID3, demote}, {?UID5, promote}]),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+%%    ?debugVal(GroupSt, 1000),
+    ExpectedGroupSt = #group_st{
+        gid = Gid,
+        action = modify_admins,
+        members = [
+            #member_st{uid = ?UID3, action = demote, type = member, result = ok},
+            #member_st{uid = ?UID2, action = promote, type = admin, result = ok},
+            #member_st{uid = ?UID5, action = promote, type = admin,
+                result = failed, reason = not_member}]
+    },
+%%    ?debugVal(ExpectedGroupSt, 1000),
+    ?assertEqual(ExpectedGroupSt, GroupSt),
+    ok.
+
+
+modify_admins_not_admin_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+    IQ = modify_admins_IQ(?UID2, Gid, [{?UID4, promote}, {?UID3, demote}]),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    Error = get_error_iq_sub_el(IQRes),
+    ?assertEqual(#stanza_error{reason = not_admin}, Error),
+    ok.
+
+
+get_group_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+%%    ?debugVal(Gid, 1000),
+    IQ = get_group_IQ(?UID1, Gid),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+%%    ?debugVal(GroupSt, 1000),
+    ExpectedGroupSt = #group_st{
+        gid = Gid,
+        name = ?GROUP_NAME1,
+        avatar = undefined,
+        members = [
+            #member_st{uid = ?UID1, type = admin},
+            #member_st{uid = ?UID2, type = member},
+            #member_st{uid = ?UID3, type = member}]
+    },
+%%    ?debugVal(ExpectedGroupSt, 1000),
+    ?assertEqual(ExpectedGroupSt, GroupSt),
+    ok.
+
+
+get_group_error_not_member_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+    IQ = get_group_IQ(?UID4, Gid),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    Error = get_error_iq_sub_el(IQRes),
+    ?assertEqual(#stanza_error{reason = not_member}, Error),
+
+    IQ2 = get_group_IQ(?UID1, <<"gdasdkjaskd">>),
+    IQRes2 = mod_groups_api:process_local_iq(IQ2),
+    Error2 = get_error_iq_sub_el(IQRes2),
+    ?assertEqual(#stanza_error{reason = not_member}, Error2),
+    ok.
+
+
+get_groups_test() ->
+    setup(),
+    Gid1 = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+    Gid2 = create_group(?UID2, ?GROUP_NAME2, [?UID1, ?UID4]),
+    IQ = get_groups_IQ(?UID1),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupsSt = get_result_iq_sub_el(IQRes),
+%%    ?debugVal(GroupsSt, 1000),
+    GroupsSet = lists:sort(GroupsSt#groups.groups),
+    ExpectedGroupsSet = lists:sort([
+        % TODO: figure out the default value for this avatar field.
+            #group_st{gid = Gid1, name = ?GROUP_NAME1, avatar = undefined},
+            #group_st{gid = Gid2, name = ?GROUP_NAME2, avatar = undefined}
+    ]),
+%%    ?debugVal(ExpectedGroupsSet, 1000),
+    ?assertEqual(ExpectedGroupsSet, GroupsSet),
+    ok.
+
+
+set_name_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+    IQ = set_name_IQ(?UID1, Gid, ?GROUP_NAME3),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    GroupSt = get_result_iq_sub_el(IQRes),
+%%    ?debugVal(GroupSt, 1000),
+    ExpectedGroupSt = #group_st{
+        gid = Gid,
+        name = ?GROUP_NAME3,
+        avatar = undefined
+    },
+%%    ?debugVal(ExpectedGroupSt, 1000),
+    ?assertEqual(ExpectedGroupSt, GroupSt),
+    ok.
+
+
+set_name_error_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, []),
+    IQ = set_name_IQ(?UID1, Gid, <<>>),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+    Error = get_error_iq_sub_el(IQRes),
+    ?assertEqual(#stanza_error{reason = invalid_name}, Error),
+
+    IQ2 = set_name_IQ(?UID2, Gid, ?GROUP_NAME2),
+    IQRes2 = mod_groups_api:process_local_iq(IQ2),
+    Error2 = get_error_iq_sub_el(IQRes2),
+    ?assertEqual(#stanza_error{reason = not_member}, Error2),
+    ok.
+
+
+leave_group_test() ->
+    setup(),
+    Gid = create_group(?UID1, ?GROUP_NAME1, [?UID2, ?UID3]),
+    IQ = leave_group_IQ(?UID2, Gid),
+    IQRes = mod_groups_api:process_local_iq(IQ),
+%%    ?debugVal(IQRes, 1000),
+    ?assertEqual(result, IQRes#iq.type),
+    ?assertEqual([], IQRes#iq.sub_els),
+    ok.
+
