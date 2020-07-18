0ac5dbdce5b683c5b75929e1b2a8ea872f202fc8
Vipin Gupta <vipin@halloapp.com>
2020-07-18 14:39:38 -0700

    CRC fix for redis

--- test/crc16_test.erl
+++ test/crc16_test.erl
@@ -18,7 +18,20 @@ crc16_redis_test() ->
     ?assertEqual(12739, crc16_redis:crc16("123456789")).
 
 crc16_test() ->
-    ?assertEqual(47933, crc16:crc16("123456789")).
+    ?assertEqual(47933, crc16:crc16("123456789")),
+    ?assertEqual(12739, ecredis_crc16:crc16("123456789")).
+
+ecredis_crc16_test() ->
+    while(254, 0, 
+        fun(X) ->
+            Random = binary_to_list(util:random_str(X)),
+            ?assertEqual(crc16_redis:crc16(Random), ecredis_crc16:crc16(Random))
+        end).
+
+while(N, N, _F) -> ok;
+while(N, M, F) ->
+    erlang:apply(F, [N]),
+    while(N -1, M, F).
 
 while(0, _F) -> ok;
 while(N, F) ->
@@ -37,8 +50,8 @@ crc16_perf_test() ->
         end),
     EndTime = util:now_ms(),
     T = EndTime - StartTime,
-%%    ?debugFmt("crc16 ~w operations took ~w ms => ~f ops ",
-%%        [N, T, N / ((T + 1) / 1000)]),
+    %% ?debugFmt("crc16 ~w operations took ~w ms => ~f ops ",
+    %%    [N, T, N / ((T + 1) / 1000)]),
     ok.
 
 
@@ -51,7 +64,7 @@ crc16_redis_perf_test() ->
         end),
     EndTime = util:now_ms(),
     T = EndTime - StartTime,
-%%    ?debugFmt("crc16_redis: ~w operations took ~w ms => ~f ops ",
-%%        [N, T, N / ((T + 1) / 1000)]),
+    %% ?debugFmt("crc16_redis: ~w operations took ~w ms => ~f ops ",
+    %%    [N, T, N / ((T + 1) / 1000)]),
     ok.
 
