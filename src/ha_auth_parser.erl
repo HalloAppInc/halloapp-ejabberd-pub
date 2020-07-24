3e35f5dabb72d6838a356a64df0454d7a8a3dbcc
Yexin Tian <yexin@halloapp.com>
2020-07-24 10:01:27 -0700

    add auth parser and test (#424)
    - add parser and tests for auth_request and auth_result

---
+++ src/ha_auth_parser.erl
@@ -0,0 +1,73 @@
+-module(ha_auth_parser).
+
+-include("packets.hrl").
+-include("logger.hrl").
+-include("xmpp.hrl").
+
+-export([
+    xmpp_to_proto/1,
+    proto_to_xmpp/1
+]).
+
+
+%% -------------------------------------------- %%
+%% XMPP to Protobuf
+%% -------------------------------------------- %%
+
+
+xmpp_to_proto(XmppAuth) -> 
+    ProtoContent = case element(1, XmppAuth) of
+        halloapp_auth -> xmpp_to_proto_auth_request(XmppAuth);
+        halloapp_auth_result -> xmpp_to_proto_auth_result(XmppAuth)
+    end, 
+    ProtoContent.
+    
+    
+xmpp_to_proto_auth_request(XmppAuth) ->
+    #pb_auth_request{
+        uid = binary_to_integer(XmppAuth#halloapp_auth.uid),
+        pwd = XmppAuth#halloapp_auth.pwd,
+        cm = #pb_client_mode{mode = XmppAuth#halloapp_auth.client_mode},
+        cv = #pb_client_version{version = XmppAuth#halloapp_auth.client_version},
+        resource = XmppAuth#halloapp_auth.resource
+    }.
+
+
+xmpp_to_proto_auth_result(XmppAuth) -> 
+    #pb_auth_result{
+        result = XmppAuth#halloapp_auth_result.result,
+        reason = XmppAuth#halloapp_auth_result.reason
+    }.
+
+
+%% -------------------------------------------- %%
+%% Protobuf to XMPP
+%% -------------------------------------------- %%
+
+
+proto_to_xmpp(ProtoAuth) ->
+    XmppAuth = case element(1, ProtoAuth) of
+        pb_auth_request -> proto_to_xmpp_auth_request(ProtoAuth);
+        pb_auth_result -> proto_to_xmpp_auth_result(ProtoAuth)
+    end, 
+    XmppAuth.    
+
+
+proto_to_xmpp_auth_request(ProtoAuth) ->
+    PbClientMode = ProtoAuth#pb_auth_request.cm,
+    PbClientVersion = ProtoAuth#pb_auth_request.cv,
+    #halloapp_auth{
+        uid = integer_to_binary(ProtoAuth#pb_auth_request.uid),
+        pwd = ProtoAuth#pb_auth_request.pwd,
+        client_mode = util:to_atom(PbClientMode#pb_client_mode.mode),
+        client_version = PbClientVersion#pb_client_version.version,
+        resource = ProtoAuth#pb_auth_request.resource
+    }.
+
+
+proto_to_xmpp_auth_result(ProtoAuth) -> 
+    #halloapp_auth_result{
+        result = ProtoAuth#pb_auth_result.result,
+        reason = ProtoAuth#pb_auth_result.reason
+    }.
+
---
+++ test/ha_auth_parser_tests.erl
@@ -0,0 +1,82 @@
+%%%-------------------------------------------------------------------
+%%% @author yexin
+%%% @copyright (C) 2020, HalloApp, Inc.
+%%% @doc
+%%%
+%%% @end
+%%% Created : 22. Jul 2020 12:00 PM
+%%%-------------------------------------------------------------------
+-module(ha_auth_parser_tests).
+-author("yexin").
+
+-include_lib("eunit/include/eunit.hrl").
+-include("packets.hrl").
+-include("xmpp.hrl").
+
+%% -------------------------------------------- %%
+%% define auth request and auth result constants
+%% -------------------------------------------- %%
+
+-define(XMPP_AUTH_REQUEST,
+    #halloapp_auth{
+        uid = <<"10093">>,
+        pwd = <<"TNpwdc">>,
+        client_mode = active, 
+        client_version = <<"2.3">>,
+        resource = android
+    }
+).
+
+-define(PB_AUTH_REQUEST,
+    #pb_auth_request{
+        uid = 10093,
+        pwd = <<"TNpwdc">>,
+        cm = #pb_client_mode{mode = active},
+        cv = #pb_client_version{version = <<"2.3">>},
+        resource = android
+    }
+).
+
+-define(XMPP_AUTH_RESULT,
+    #halloapp_auth_result{
+        result = <<"Success!">>,
+        reason = <<"none">>
+    }
+).
+
+-define(PB_AUTH_RESULT,
+    #pb_auth_result{
+        result = <<"Success!">>,
+        reason = <<"none">>
+    }
+).
+
+
+%% -------------------------------------------- %%
+%% internal tests
+%% -------------------------------------------- %%
+
+
+xmpp_to_proto_auth_request_test() -> 
+    ProtoAuth = ha_auth_parser:xmpp_to_proto(?XMPP_AUTH_REQUEST),
+    ?assertEqual(true, is_record(ProtoAuth, pb_auth_request)),
+    ?assertEqual(?PB_AUTH_REQUEST, ProtoAuth).
+
+
+proto_to_xmpp_auth_request_test() -> 
+    XmppAuth = ha_auth_parser:proto_to_xmpp(?PB_AUTH_REQUEST),
+    ?assertEqual(true, is_record(XmppAuth, halloapp_auth)),
+    ?assertEqual(?XMPP_AUTH_REQUEST, XmppAuth).
+
+
+xmpp_to_proto_auth_result_test() -> 
+    ProtoAuth = ha_auth_parser:xmpp_to_proto(?XMPP_AUTH_RESULT),
+    ?assertEqual(true, is_record(ProtoAuth, pb_auth_result)),
+    ?assertEqual(?PB_AUTH_RESULT, ProtoAuth).
+
+
+proto_to_xmpp_auth_result_test() -> 
+    XmppAuth = ha_auth_parser:proto_to_xmpp(?PB_AUTH_RESULT),
+    ?assertEqual(true, is_record(XmppAuth, halloapp_auth_result)),
+    ?assertEqual(?XMPP_AUTH_RESULT, XmppAuth).
+
