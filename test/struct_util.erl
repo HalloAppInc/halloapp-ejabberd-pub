7a4aa9f107bdf187e8ecb9790a23fae0d182a619
murali <murali@halloapp.com>
2021-01-04 11:42:40 -0800

    Revert "Include sender_log_info in pb_chat stanza (#1081)"
    This reverts commit 94f60cf88ce52728ad123a081d24b8a5cb963715.

--- test/struct_util.erl
+++ test/struct_util.erl
@@ -650,30 +650,21 @@ create_enc_xmlel(Data, IdentityKey, OneTimeKeyId) ->
 
 
 create_chat_stanza(Timestamp, SenderName, SubEls) ->
-    create_chat_stanza(Timestamp, SenderName, SubEls, undefined).
-
-
-create_chat_stanza(Timestamp, SenderName, SubEls, SenderLogInfo) ->
     #chat{
         xmlns = <<"halloapp:chat:messages">>,
         timestamp = Timestamp,
         sender_name = SenderName,
-        sub_els = SubEls,
-        sender_log_info = SenderLogInfo
+        sub_els = SubEls
     }.
 
 create_pb_chat_stanza(Timestamp, SenderName, Payload, EncPayload, PublicKey, OneTimeKeyId) ->
-    create_pb_chat_stanza(Timestamp, SenderName, Payload, EncPayload, PublicKey, OneTimeKeyId, undefined).
-
-create_pb_chat_stanza(Timestamp, SenderName, Payload, EncPayload, PublicKey, OneTimeKeyId, SenderLogInfo) ->
     #pb_chat_stanza{
         timestamp = Timestamp,
         sender_name = SenderName,
         payload = Payload,
         enc_payload = EncPayload,
         public_key = PublicKey,
-        one_time_pre_key_id = OneTimeKeyId,
-        sender_log_info = SenderLogInfo
+        one_time_pre_key_id = OneTimeKeyId
     }.
 
 
