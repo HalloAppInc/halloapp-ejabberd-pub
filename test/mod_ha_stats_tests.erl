b523e803db1cea2b22e00f4e6bfa00ed540958ff
Nikola Borisov <nikola@halloapp.com>
2021-02-03 14:10:55 -0800

    Set bigger timeout for the starting prometheus.
    For some reason starting prometheus takes 2 sec.

--- test/mod_ha_stats_tests.erl
+++ test/mod_ha_stats_tests.erl
@@ -18,50 +18,49 @@
 %% Tests
 %%====================================================================
 
+start_prometheus_test_() ->
+    {timeout, 10,
+        fun() ->
+            setup(),
+        end}.
 
 new_user_stat_test() ->
-    {timeout, 15,
-        fun() ->
-            meck:new(prometheus_histogram, [merge_expects, passthrough]),
-            setup(),
-            mod_ha_stats:register_user(?UID1, ?SERVER, <<"">>),
-            mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 3, 10),
-            mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 1, 5),
-            mod_ha_stats:log_new_user(?UID1),
-            timer:sleep(10),
-            {_, NumPosts} = prometheus_histogram:value(ha_new_user_initial_feed_posts),
-            {_, NumComments} = prometheus_histogram:value(ha_new_user_initial_feed_comments),
-            ?assertEqual(4, NumPosts),
-            ?assertEqual(15, NumComments),
-            ?assert(meck:validate(prometheus_histogram)),
-            meck:unload(prometheus_histogram),
-            ok
-        end}.
+    meck:new(prometheus_histogram, [merge_expects, passthrough]),
+    setup(),
+    mod_ha_stats:register_user(?UID1, ?SERVER, <<"">>),
+    mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 3, 10),
+    mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 1, 5),
+    mod_ha_stats:log_new_user(?UID1),
+    timer:sleep(10),
+    {_, NumPosts} = prometheus_histogram:value(ha_new_user_initial_feed_posts),
+    {_, NumComments} = prometheus_histogram:value(ha_new_user_initial_feed_comments),
+    ?assertEqual(4, NumPosts),
+    ?assertEqual(15, NumComments),
+    ?assert(meck:validate(prometheus_histogram)),
+    meck:unload(prometheus_histogram),
+    ok.
 
 new_user_stat_log_after_share_test() ->
-    {timeout, 15,
-        fun() ->
-            meck:new(prometheus_histogram, [merge_expects, passthrough]),
-            prometheus_histogram:reset(ha_new_user_initial_feed_posts),
-            prometheus_histogram:reset(ha_new_user_initial_feed_comments),
-            %setup(),
-            mod_ha_stats:register_user(?UID1, ?SERVER, <<"">>),
-            mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 3, 10),
-            mod_ha_stats:log_new_user(?UID1),
-            % This data will not be counted since it happened after the log_new_user
-            mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 1, 5),
-            timer:sleep(10),
-            {_, NumPosts} = prometheus_histogram:value(ha_new_user_initial_feed_posts),
-            {_, NumComments} = prometheus_histogram:value(ha_new_user_initial_feed_comments),
-            ?assertEqual(3, NumPosts),
-            ?assertEqual(10, NumComments),
-            ?assert(meck:validate(prometheus_histogram)),
-            meck:unload(prometheus_histogram),
-            ok
-        end}.
+    meck:new(prometheus_histogram, [merge_expects, passthrough]),
+    prometheus_histogram:reset(ha_new_user_initial_feed_posts),
+    prometheus_histogram:reset(ha_new_user_initial_feed_comments),
+    setup(),
+    mod_ha_stats:register_user(?UID1, ?SERVER, <<"">>),
+    mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 3, 10),
+    mod_ha_stats:log_new_user(?UID1),
+    % This data will not be counted since it happened after the log_new_user
+    mod_ha_stats:feed_share_old_items(?UID2, ?UID1, 1, 5),
+    timer:sleep(10),
+    {_, NumPosts} = prometheus_histogram:value(ha_new_user_initial_feed_posts),
+    {_, NumComments} = prometheus_histogram:value(ha_new_user_initial_feed_comments),
+    ?assertEqual(3, NumPosts),
+    ?assertEqual(10, NumComments),
+    ?assert(meck:validate(prometheus_histogram)),
+    meck:unload(prometheus_histogram),
+    ok.
 
 setup() ->
     application:ensure_started(prometheus),
-    {ok, _Pid} = mod_ha_stats:start_link(),
+    mod_ha_stats:start_link(),
     ok.
 
