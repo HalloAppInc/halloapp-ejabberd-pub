d3c10508b7f643a246cc7eb107beec918e188a9a
murali <murali@halloapp.com>
2021-02-22 17:14:35 -0800

    HOT FIX: Fix crash in compute_counts in stats

--- src/stat.erl
+++ src/stat.erl
@@ -202,23 +202,27 @@ compute_counts_by_version() ->
             maps:put(Version, CountsByVersion, Acc)
         end, #{}, Versions),
 
-    {ok, NewVersionCountsMap} = model_accounts:count_version_keys(),
-    lists:foreach(
-        fun(Version) ->
-            Count1 = maps:get(Version, NewVersionCountsMap, 0),
-            Count2 = maps:get(Version, OldVersionCountsMap, 0),
-            case Count1 =:= Count2 of
-                false ->
-                    ?INFO("Counters dont match for version: ~p, count1: ~p, count2: ~p",
-                            [Version, Count1, Count2]);
-                true -> ok
-            end
-        end, Versions),
-
-    %% Cleanup old version fields if any.
-    ExistingVersions = maps:keys(NewVersionCountsMap),
-    DeleteVersions = lists:subtract(ExistingVersions, Versions),
-    ok = model_accounts:cleanup_version_keys(DeleteVersions),
+    case persistent_term:get("version_counters", true) of
+        true ->
+            NewVersionCountsMap = model_accounts:count_version_keys(),
+            lists:foreach(
+                fun(Version) ->
+                    Count1 = maps:get(Version, NewVersionCountsMap, 0),
+                    Count2 = maps:get(Version, OldVersionCountsMap, 0),
+                    case Count1 =:= Count2 of
+                        false ->
+                            ?INFO("Counters dont match for version: ~p, count1: ~p, count2: ~p",
+                                    [Version, Count1, Count2]);
+                        true -> ok
+                    end
+                end, Versions),
+            %% Cleanup old version fields if any.
+            ExistingVersions = maps:keys(NewVersionCountsMap),
+            DeleteVersions = lists:subtract(ExistingVersions, Versions),
+            ok = model_accounts:cleanup_version_keys(DeleteVersions);
+        false ->
+            ok
+    end,
 
     End = util:now_ms(),
     ?INFO("Counting took ~p ms", [End - Start]),
