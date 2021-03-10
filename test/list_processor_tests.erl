14e07a871eac8702ea1e047ba127e367850f50c6
shashank-halloapp <shashank@halloapp.com>
2021-03-11 01:01:11 +0530

    uncomment list_processor test

--- test/list_processor_tests.erl
+++ test/list_processor_tests.erl
@@ -56,65 +56,64 @@ clear() ->
     tutil:cleardb(redis_phone),
     tutil:cleardb(redis_accounts).
 
-%TODO: this test fails on Mac OS. Re-enable once we figure out how to fix it.
-%%check_file_processing_test() ->
-%%    try
-%%        file:delete(?TEMP_FILE_NAME)
-%%    catch _ ->
-%%        ?debugFmt("Unable to delete: ~s. It is ok.", [?TEMP_FILE_NAME])
-%%    end,
-%%
-%%    {ok, Fh} = file:open(?TEMP_FILE_NAME, [write]),
-%%    io:format(Fh, "~p~n", [?UID1]),
-%%    io:format(Fh, "~p~n", [?UID2]),
-%%    io:format(Fh, "~p~n", [?UID3]),
-%%    io:format(Fh, "~p~n", [?UID4]),
-%%    io:format(Fh, "~p~n", [?UID5]),
-%%    file:close(Fh),
-%%    ?assertEqual(1, list_processor:process_file_list(?TEMP_FILE_NAME, fun is_even/1, 1)),
-%%    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun is_even/1, 2)),
-%%    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun is_even/1, 10)),
-%%    ok = file:delete(?TEMP_FILE_NAME),
-%%
-%%    setup(),
-%%    {ok, Uid1, register} = ejabberd_auth:check_and_register(?PHONE1, ?SERVER, ?PASS, ?NAME1, ?UA),
-%%    {ok, Uid2, register} = ejabberd_auth:check_and_register(?PHONE2, ?SERVER, ?PASS, ?NAME2, ?UA),
-%%    {ok, Uid3, register} = ejabberd_auth:check_and_register(?PHONE3, ?SERVER, ?PASS, ?NAME3, ?UA),
-%%    {ok, Uid4, register} = ejabberd_auth:check_and_register(?PHONE4, ?SERVER, ?PASS, ?NAME4, ?UA),
-%%    {ok, Uid5, register} = ejabberd_auth:check_and_register(?PHONE5, ?SERVER, ?PASS, ?NAME5, ?UA),
-%%
-%%    {ok, Fh2} = file:open(?TEMP_FILE_NAME, [write]),
-%%    io:format(Fh2, "~p~n", [Uid1]),
-%%    io:format(Fh2, "~p~n", [Uid2]),
-%%    io:format(Fh2, "~p~n", [Uid3]),
-%%    io:format(Fh2, "~p~n", [Uid4]),
-%%    io:format(Fh2, "~p~n", [Uid5]),
-%%    file:close(Fh2),
-%%    ?assertEqual(1, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 1)),
-%%    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 2)),
-%%    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 10)),
-%%    ?assertEqual(0, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 10)),
-%%    ok = file:delete(?TEMP_FILE_NAME),
-%%
-%%    %% Register again.
-%%    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE1, ?SERVER, ?PASS, ?NAME1, ?UA),
-%%    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE2, ?SERVER, ?PASS, ?NAME2, ?UA),
-%%    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE3, ?SERVER, ?PASS, ?NAME3, ?UA),
-%%    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE4, ?SERVER, ?PASS, ?NAME4, ?UA),
-%%    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE5, ?SERVER, ?PASS, ?NAME5, ?UA),
-%%
-%%    {ok, Fh3} = file:open(?TEMP_FILE_NAME, [write]),
-%%    io:format(Fh3, "~p~n", [?PHONE1]),
-%%    io:format(Fh3, "~p~n", [?PHONE2]),
-%%    io:format(Fh3, "~p~n", [?PHONE3]),
-%%    io:format(Fh3, "~p~n", [?PHONE4]),
-%%    io:format(Fh3, "~p~n", [?PHONE5]),
-%%    io:format(Fh3, "~p~n", [?PHONE6]),
-%%    io:format(Fh3, "~p~n", [?PHONE7]),
-%%    io:format(Fh3, "~p~n", [?PHONE8]),
-%%    file:close(Fh3),
-%%    ?assertEqual(1, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:validate_phone/1, 1)),
-%%    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:validate_phone/1, 2)),
-%%    ?assertEqual(5, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:validate_phone/1, 10)),
-%%    ok = file:delete(?TEMP_FILE_NAME).
+check_file_processing_test() ->
+    try
+        file:delete(?TEMP_FILE_NAME)
+    catch _ ->
+        ?debugFmt("Unable to delete: ~s. It is ok.", [?TEMP_FILE_NAME])
+    end,
+
+    {ok, Fh} = file:open(?TEMP_FILE_NAME, [write]),
+    io:format(Fh, "~p~n", [?UID1]),
+    io:format(Fh, "~p~n", [?UID2]),
+    io:format(Fh, "~p~n", [?UID3]),
+    io:format(Fh, "~p~n", [?UID4]),
+    io:format(Fh, "~p~n", [?UID5]),
+    file:close(Fh),
+    ?assertEqual(1, list_processor:process_file_list(?TEMP_FILE_NAME, fun is_even/1, 1)),
+    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun is_even/1, 2)),
+    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun is_even/1, 10)),
+    ok = file:delete(?TEMP_FILE_NAME),
+
+    setup(),
+    {ok, Uid1, register} = ejabberd_auth:check_and_register(?PHONE1, ?SERVER, ?PASS, ?NAME1, ?UA),
+    {ok, Uid2, register} = ejabberd_auth:check_and_register(?PHONE2, ?SERVER, ?PASS, ?NAME2, ?UA),
+    {ok, Uid3, register} = ejabberd_auth:check_and_register(?PHONE3, ?SERVER, ?PASS, ?NAME3, ?UA),
+    {ok, Uid4, register} = ejabberd_auth:check_and_register(?PHONE4, ?SERVER, ?PASS, ?NAME4, ?UA),
+    {ok, Uid5, register} = ejabberd_auth:check_and_register(?PHONE5, ?SERVER, ?PASS, ?NAME5, ?UA),
+
+    {ok, Fh2} = file:open(?TEMP_FILE_NAME, [write]),
+    io:format(Fh2, "~p~n", [Uid1]),
+    io:format(Fh2, "~p~n", [Uid2]),
+    io:format(Fh2, "~p~n", [Uid3]),
+    io:format(Fh2, "~p~n", [Uid4]),
+    io:format(Fh2, "~p~n", [Uid5]),
+    file:close(Fh2),
+    ?assertEqual(1, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 1)),
+    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 2)),
+    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 10)),
+    ?assertEqual(0, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:remove_user/1, 10)),
+    ok = file:delete(?TEMP_FILE_NAME),
+
+    %% Register again.
+    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE1, ?SERVER, ?PASS, ?NAME1, ?UA),
+    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE2, ?SERVER, ?PASS, ?NAME2, ?UA),
+    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE3, ?SERVER, ?PASS, ?NAME3, ?UA),
+    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE4, ?SERVER, ?PASS, ?NAME4, ?UA),
+    {ok, _, register} = ejabberd_auth:check_and_register(?PHONE5, ?SERVER, ?PASS, ?NAME5, ?UA),
+
+    {ok, Fh3} = file:open(?TEMP_FILE_NAME, [write]),
+    io:format(Fh3, "~p~n", [?PHONE1]),
+    io:format(Fh3, "~p~n", [?PHONE2]),
+    io:format(Fh3, "~p~n", [?PHONE3]),
+    io:format(Fh3, "~p~n", [?PHONE4]),
+    io:format(Fh3, "~p~n", [?PHONE5]),
+    io:format(Fh3, "~p~n", [?PHONE6]),
+    io:format(Fh3, "~p~n", [?PHONE7]),
+    io:format(Fh3, "~p~n", [?PHONE8]),
+    file:close(Fh3),
+    ?assertEqual(1, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:validate_phone/1, 1)),
+    ?assertEqual(2, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:validate_phone/1, 2)),
+    ?assertEqual(5, list_processor:process_file_list(?TEMP_FILE_NAME, fun list_processor:validate_phone/1, 10)),
+    ok = file:delete(?TEMP_FILE_NAME).
 
