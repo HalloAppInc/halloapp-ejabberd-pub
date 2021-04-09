f13e87a14428c8ac8a7eedfd55b299330d8e9194
Vipin Gupta <vipin@halloapp.com>
2021-04-08 19:12:43 -0700

    Ability to send OTP via voice call from MessageBird

--- include/mbird.hrl
+++ include/mbird.hrl
@@ -9,7 +9,8 @@
 -ifndef(MBIRD_HRL).
 -define(MBIRD_HRL, true).
 
--define(BASE_URL, "https://rest.messagebird.com/messages").
+-define(BASE_SMS_URL, "https://rest.messagebird.com/messages").
+-define(BASE_VOICE_URL, "https://voice.messagebird.com/calls").
 -define(FROM_PHONE_FOR_CANADA, "+12262403383").
 -define(FROM_PHONE_FOR_REST, "+12022213975").
 -define(REFERENCE, "HALLOAPP").
--- src/mbird.erl
+++ src/mbird.erl
@@ -15,13 +15,15 @@
 %% API
 -export([
     send_sms/2,
-    normalized_status/1
+    send_voice_call/2,
+    normalized_status/1,
+    compose_voice_body/2  %% for debugging
 ]).
 
 -spec send_sms(Phone :: phone(), Msg :: string()) -> {ok, sms_response()} | {error, sms_fail}.
 send_sms(Phone, Msg) ->
     ?INFO("Phone: ~p, Msg: ~s", [Phone, Msg]),
-    URL = ?BASE_URL,
+    URL = ?BASE_SMS_URL,
     Headers = [{"Authorization", "AccessKey " ++ get_access_key()}],
     Type = "application/x-www-form-urlencoded",
     Body = compose_body(Phone, Msg),
@@ -41,6 +43,30 @@ send_sms(Phone, Msg) ->
         _ ->
             ?ERROR("Sending SMS failed ~p", [Response]),
             {error, sms_fail}
+    end.
+
+-spec send_voice_call(Phone :: phone(), Msg :: string()) -> {ok, sms_response()} | {error, voice_call_fail}.
+send_voice_call(Phone, Msg) ->
+    ?INFO("Phone: ~p, Msg: ~s", [Phone, Msg]),
+    URL = ?BASE_VOICE_URL,
+    Headers = [{"Authorization", "AccessKey " ++ get_access_key()}],
+    Type = "application/json",
+    Body = compose_voice_body(Phone, Msg),
+    ?DEBUG("Body: ~p", [Body]),
+    HTTPOptions = [],
+    Options = [],
+    Response = httpc:request(post, {URL, Headers, Type, Body}, HTTPOptions, Options),
+    ?DEBUG("Response: ~p", [Response]),
+    case Response of
+        {ok, {{_, 201, _}, _ResHeaders, ResBody}} ->
+            Json = jiffy:decode(ResBody, [return_maps]),
+            [Data] = maps:get(<<"data">>, Json),
+            Id = maps:get(<<"id">>, Data),
+            Status = normalized_status(maps:get(<<"status">>, Data)),
+            {ok, #sms_response{sms_id = Id, status = Status, response = ResBody}};
+        _ ->
+            ?ERROR("Sending Voice Call failed ~p", [Response]),
+            {error, voice_call_fail}
     end.
 
 -spec normalized_status(Status :: binary()) -> atom().
@@ -70,10 +96,7 @@ get_access_key() ->
     Body :: uri_string:uri_string().
 compose_body(Phone, Message) ->
     PlusPhone = "+" ++ binary_to_list(Phone),
-    FromPhone = case mod_libphonenumber:get_cc(Phone) of
-        <<"CA">> -> ?FROM_PHONE_FOR_CANADA;
-        _ -> ?FROM_PHONE_FOR_REST
-    end,
+    FromPhone = get_from_phone(Phone),
     %% reference is used during callback. TODO(vipin): Need a more useful ?REFERENCE.
     uri_string:compose_query([
         {"recipients", PlusPhone },
@@ -81,4 +104,37 @@ compose_body(Phone, Message) ->
         {"reference", ?REFERENCE},
         {"body", Message}
     ], [{encoding, utf8}]).
+
+-spec compose_voice_body(Phone, Message) -> Body when
+    Phone :: phone(),
+    Message :: string(),
+    Body :: uri_string:uri_string().
+compose_voice_body(Phone, Message) ->
+    PlusPhone = "+" ++ binary_to_list(Phone),
+    FromPhone = get_from_phone(Phone),
+    %% TODO(vipin): 1. Add the callback.
+    %% Ref: https://developers.messagebird.com/api/voice-calling/#calls
+    Body = #{
+        <<"source">> => list_to_binary(FromPhone),
+        <<"destination">> => list_to_binary(PlusPhone),
+        <<"callFlow">> => #{
+            <<"title">> => <<"Say message">>,
+            <<"steps">> => [#{
+                <<"action">> => <<"say">>,
+                <<"options">> => #{
+                    <<"payload">> => list_to_binary(Message),
+                    <<"voice">> => <<"male">>,
+                    <<"language">> => <<"en-US">>
+                }
+            }]
+        }
+    },
+    binary_to_list(jiffy:encode(Body)).
+
+
+get_from_phone(Phone) ->
+    case mod_libphonenumber:get_cc(Phone) of
+        <<"CA">> -> ?FROM_PHONE_FOR_CANADA;
+        _ -> ?FROM_PHONE_FOR_REST
+    end.
 
