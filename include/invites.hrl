86e922b9be6f7610a0930ed3bf0be691ead6d134
Nikola Borisov <nikola@halloapp.com>
2021-07-06 12:37:14 -0700

    Revert "Open up registration and invite system."
    This reverts commit 47e2c6900e62746cc3fb36906375e849a8b48b6e.

--- include/invites.hrl
+++ include/invites.hrl
@@ -1,9 +1,6 @@
 -ifndef(INVITES_HRL).
-
 -define(INVITES_HRL, 1).
+
 -define(MAX_NUM_INVITES, 5).
-% Controls if registrations are allowed without invite and the number of invites a user gets
--define(IS_INVITE_REQUIRED, false).
--define(INF_INVITES, 10000).
 
 -endif.
