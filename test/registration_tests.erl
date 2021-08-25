ed54e5babd995a8a3521fc322b3e813ca9363e71
Murali Balusu <58709405+balusu-murali@users.noreply.github.com>
2021-08-25 12:24:00 -0700

    Update unknown in protobuf enums (#2464)
    * Update unknown in protobuf enums
    
    - update proto defs based on changes in schemas repo.

--- test/registration_tests.erl
+++ test/registration_tests.erl
@@ -150,7 +150,7 @@ request_otp_noise_test(_Conf) ->
         response = #pb_otp_response{
             phone = Phone,
             result = success,
-            reason = unknown,
+            reason = unknown_reason,
             retry_after_secs = 30
     }},
     % ?debugFmt("response: ~p", [ActualResponse]),
@@ -207,7 +207,7 @@ request_otp_noise2_test(_Conf) ->
         response = #pb_otp_response{
             phone = Phone,
             result = success,
-            reason = unknown,
+            reason = unknown_reason,
             retry_after_secs = 30
     }},
     % ?debugFmt("response: ~p", [ActualResponse]),
