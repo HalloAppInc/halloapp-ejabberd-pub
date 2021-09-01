baa3637c547a1b3549d6ee2b3c28a2910ebfa7cb
balusu-murali <murali@halloapp.com>
2021-09-01 10:54:37 -0700

    HOT FIX: fix crash in stat process

--- src/stat.erl
+++ src/stat.erl
@@ -209,9 +209,9 @@ compute_counts_by_version() ->
         fun(Version) ->
             CountsByVersion = maps:get(Version, VersionCountsMap, 0),
             Platform = util_ua:get_client_type(Version),
-            IsValid = util:to_list(maps:is_key(Version, ValidVersionsMap)),
+            IsValid = maps:is_key(Version, ValidVersionsMap),
             stat:gauge("HA/client_version", "all_users", CountsByVersion,
-                    [{version, Version}, {platform, Platform}, [{valid, IsValid}]])
+                    [{version, Version}, {platform, Platform}, {valid, IsValid}])
         end, AllVersions),
     End = util:now_ms(),
     ?INFO("Counting took ~p ms", [End - Start]),
