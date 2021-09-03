72085e637e46827e0f586f3e84fc1a50b65728ef
Nikola Borisov <nikola@halloapp.com>
2021-09-03 16:31:51 -0700

    Infobip sms gateway (#2509)
    Send SMS implementation for infobip.
    Log Error for voice call

---
+++ src/infobip.erl
@@ -0,0 +1,166 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2021, HalloApp, Inc
+%%% @doc
+%%% SMS module with helper functions to send SMS messages.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(infobip).
+-behavior(mod_sms).
+-author(nikola).
+-include("logger.hrl").
+-include("ha_types.hrl").
+-include("sms.hrl").
+
+%% API
+-export([
+    init/0,
+    can_send_sms/1,
+    can_send_voice_call/1,
+    send_sms/4,
+    send_voice_call/4,
+    normalized_status/1,
+    get_api_key/0,
+    send_feedback/2,
+    compose_body/2     %% for debugging
+]).
+
+-define(BASE_SMS_URL, "https://k36v11.api.infobip.com/sms/2/text/advanced").
+
+init() ->
+    ok.
+%%    FromPhoneList = [],
+%%    util_sms:init_helper(infobip_options, FromPhoneList).
+
+
+-spec can_send_sms(CC :: binary()) -> boolean().
+can_send_sms(_CC) ->
+    false.
+-spec can_send_voice_call(CC :: binary()) -> boolean().
+can_send_voice_call(_CC) ->
+    false.
+
+%%Example request
+%%POST /sms/2/text/single
+%%Host: k36v11.api.infobip.com
+%%Authorization: App 86...
+%%Content-Type: application/json
+%%Accept: application/json
+%%
+%%{
+%%"from":"447491163443",
+%%"to":"12066585586",
+%%"text":"Hello Nikola"
+%%}
+
+%% Example response
+%%{
+%%"messages": [
+%%{
+%%"to": "12066585586",
+%%"status": {
+%%"groupId": 1,
+%%"groupName": "PENDING",
+%%"id": 26,
+%%"name": "PENDING_ACCEPTED",
+%%"description": "Message sent to next instance"
+%%},
+%%"messageId": "33053596995305284925"
+%%}
+%%]
+%%}
+-spec send_sms(Phone :: phone(), Code :: binary(), LangId :: binary(), UserAgent :: binary()) ->
+    {ok, gateway_response()} | {error, sms_fail, retry | no_retry}.
+send_sms(Phone, Code, LangId, UserAgent) ->
+    {SmsMsgBin, _TranslatedLangId} = mod_translate:translate(<<"server.sms.verification">>, LangId),
+    AppHash = util_ua:get_app_hash(UserAgent),
+    Msg = io_lib:format("~s: ~s~n~n~n~s", [SmsMsgBin, Code, AppHash]),
+
+    ?INFO("Phone: ~p, Msg: ~p", [Phone, Msg]),
+    URL = ?BASE_SMS_URL,
+    Headers = [
+        {"Authorization", "App " ++ get_api_key()},
+        {"Content-Type", "application/json"},
+        {"Accept", "application/json"}
+    ],
+    Type = "application/json",
+    Body = compose_body(Phone, Msg),
+    ?DEBUG("Body: ~p", [Body]),
+    HTTPOptions = [],
+    Options = [],
+    Response = httpc:request(post, {URL, Headers, Type, Body}, HTTPOptions, Options),
+    ?DEBUG("Response: ~p", [Response]),
+    case Response of
+        {ok, {{_, 200, _}, _ResHeaders, ResBody}} ->
+            Json = jiffy:decode(ResBody, [return_maps]),
+            [Message] = maps:get(<<"messages">>, Json),
+            Id = maps:get(<<"messageId">>, Message),
+            StatusObj = maps:get(<<"status">>, Message),
+            StatusGroupName = maps:get(<<"groupName">>, StatusObj),
+            StatusName = maps:get(<<"name">>, StatusObj),
+            StatusDesc = maps:get(<<"description">>, StatusObj),
+            Status = normalized_status(StatusGroupName),
+            OkResponse = {ok, #gateway_response{gateway_id = Id, status = Status, response = ResBody}},
+            FailedResponse = {error, sms_fail, retry},
+            ?INFO("SMS to Phone:~p gw:infobip ~p:~p Id:~p",
+                [Phone, StatusGroupName, StatusName, Id]),
+            case Status of
+                accepted -> OkResponse;
+                delivered -> OkResponse;
+                _ ->
+                    ?INFO("Sending SMS failed Phone:~p ~p:~p Desc:~p ResBody:~p",
+                        [Phone, StatusGroupName, StatusName, StatusDesc, ResBody]),
+                    FailedResponse
+            end;
+        {ok, {{_, ResponseCode, _}, _ResHeaders, _ResBody}} when ResponseCode >= 400 ->
+            ?ERROR("Sending SMS failed, Code ~p, response ~p (retry)", [ResponseCode, Response]),
+            {error, sms_fail, retry};
+        _ ->
+            % TODO: we should retry this case
+            ?ERROR("Sending SMS failed (no_retry) ~p", [Response]),
+            {error, sms_fail, no_retry}
+    end.
+
+-spec send_voice_call(Phone :: phone(), Code :: binary(), LangId :: binary(), UserAgent :: binary()) ->
+    {ok, gateway_response()} | {error, voice_call_fail, retry | no_retry}.
+send_voice_call(Phone, _Code, _LangId, _UserAgent) ->
+    ?ERROR("Infobip voice calls are not implemented. Phone: ~s", [Phone]),
+    {error, voice_call_fail, retry}.
+
+-spec normalized_status(Status :: binary()) -> atom().
+normalized_status(<<"PENDING">>) ->
+    accepted;
+normalized_status(<<"UNDELIVERABLE">>) ->
+    undelivered;
+normalized_status(<<"DELIVERED">>) ->
+    delivered;
+normalized_status(<<"EXPIRED">>) ->
+    failed;
+normalized_status(<<"REJECTED">>) ->
+    failed;
+normalized_status(_) ->
+    unknown.
+
+-spec get_api_key() -> string().
+get_api_key() ->
+    Json = jiffy:decode(binary_to_list(mod_aws:get_secret(<<"Infobip">>)), [return_maps]),
+    binary_to_list(maps:get(<<"api_key">>, Json)).
+
+-spec compose_body(Phone, Message) -> Body when
+    Phone :: phone(),
+    Message :: string(),
+    Body :: iodata().
+compose_body(Phone, Message) ->
+    PlusPhone = "+" ++ binary_to_list(Phone),
+    jiffy:encode(#{
+        messages => [#{
+            from => <<"HalloApp">>, % TODO: maybe do phone numbers?
+            destinations => [#{to => util:to_binary(PlusPhone)}],
+            text => util:to_binary(Message)
+    }]}).
+
+
+% Todo: Implement if sending feedback back to mbird
+-spec send_feedback(Phone :: phone(), AllVerifyInfo :: list()) -> ok.
+send_feedback(_Phone, _AllVerifyInfo) ->
+    ok.
+
