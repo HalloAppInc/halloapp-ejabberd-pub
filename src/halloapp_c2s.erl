54f53597725d61e1a13b868c8f9de1cf219febd8
Murali Balusu <58709405+balusu-murali@users.noreply.github.com>
2021-09-24 13:40:58 -0700

    Disable upgrading contact_list stanzas (#2669)
    - proto changes have been deployed everywhere: so we can now disable them.

--- src/halloapp_c2s.erl
+++ src/halloapp_c2s.erl
@@ -200,47 +200,47 @@ open_session(#{user := U, server := S, resource := R, sid := SID, client_version
 %% then other servers cant encode this message because the record has a new field. 
 %% similarly the updated server cant encode it because it is missing a field.
 %% so this function helps us transform packets across servers.
-upgrade_packet(#pb_msg{payload = MsgPayload} = Msg) ->
-    case MsgPayload of
-        #pb_contact{} -> Msg;
-        %% pb_contact
-        {pb_contact, Action, Raw, Normalized, Uid, AvatarId, Name, NumPotentialFriends} ->
-            NewMsgPayload = #pb_contact{
-                action = Action,
-                raw = Raw,
-                normalized = Normalized,
-                uid = Uid,
-                avatar_id = AvatarId,
-                name = Name,
-                num_potential_friends = NumPotentialFriends
-            },
-            Msg#pb_msg{payload = NewMsgPayload};
-        %% pb_contact
-        {pb_contact, Action, Raw, Normalized, Uid, AvatarId, Role, Name, NumPotentialFriends} ->
-            NewMsgPayload = #pb_contact{
-                action = Action,
-                raw = Raw,
-                normalized = Normalized,
-                uid = Uid,
-                avatar_id = AvatarId,
-                name = Name,
-                num_potential_friends = NumPotentialFriends
-            },
-            Msg#pb_msg{payload = NewMsgPayload};
-        _ -> Msg
-    end;
+% upgrade_packet(#pb_msg{payload = MsgPayload} = Msg) ->
+%     case MsgPayload of
+%         #pb_contact{} -> Msg;
+%         %% pb_contact
+%         {pb_contact, Action, Raw, Normalized, Uid, AvatarId, Name, NumPotentialFriends} ->
+%             NewMsgPayload = #pb_contact{
+%                 action = Action,
+%                 raw = Raw,
+%                 normalized = Normalized,
+%                 uid = Uid,
+%                 avatar_id = AvatarId,
+%                 name = Name,
+%                 num_potential_friends = NumPotentialFriends
+%             },
+%             Msg#pb_msg{payload = NewMsgPayload};
+%         %% pb_contact
+%         {pb_contact, Action, Raw, Normalized, Uid, AvatarId, Role, Name, NumPotentialFriends} ->
+%             NewMsgPayload = #pb_contact{
+%                 action = Action,
+%                 raw = Raw,
+%                 normalized = Normalized,
+%                 uid = Uid,
+%                 avatar_id = AvatarId,
+%                 name = Name,
+%                 num_potential_friends = NumPotentialFriends
+%             },
+%             Msg#pb_msg{payload = NewMsgPayload};
+%         _ -> Msg
+%     end;
 upgrade_packet(Packet) -> Packet.
 
 
 process_info(#{lserver := LServer} = State, {route, Packet}) ->
     NewPacket = upgrade_packet(Packet),
     %% TODO: Remove enif_protobuf:encode(...) after upgrade is done.
-    case enif_protobuf:encode(NewPacket) of
-        {error, Reason} ->
-            ?ERROR("Error encoding packet: ~p, reason: ~p, Orig: ~p", [NewPacket, Reason, Packet]);
-        _ ->
-            ok
-    end,
+    % case enif_protobuf:encode(NewPacket) of
+    %     {error, Reason} ->
+    %         ?ERROR("Error encoding packet: ~p, reason: ~p, Orig: ~p", [NewPacket, Reason, Packet]);
+    %     _ ->
+    %         ok
+    % end,
     case verify_incoming_packet(State, NewPacket) of
         allow ->
             %% TODO(murali@): remove temp counts after clients transition.
