1cb2c2938bb8e5f3eb708f8e501effef5f4e3acb
Murali Balusu <58709405+balusu-murali@users.noreply.github.com>
2021-10-06 08:25:25 -0700

    Store push-ids for a week to avoid duplicate push notifications (#2706)
    - i am concerned with the memory usage for this.
    - but i plan to have this only until the end of this year and we can then remove this code altogether.
    - this would help solve some brief double push issues we have been noticing with groupfeed e2e rerequests.

--- include/expiration.hrl
+++ include/expiration.hrl
@@ -12,6 +12,11 @@
 
 -include("time.hrl").
 
--define(PUSH_EXPIRATION, (1 * ?DAYS)).
+%% TODO (murali@): Remove tracking push notifications being sent to the client.
+%% i am concerned with the memory usage for this, but we only need this for about 3 more months
+%% until ios can handle dismissing notifications for all content.
+%% We can remove this code altogether then. this would help solve some duplicate 
+%% push issues we have been noticing with groupfeed e2e rerequests.
+-define(PUSH_EXPIRATION, (7 * ?DAYS)).
 
 -endif.
