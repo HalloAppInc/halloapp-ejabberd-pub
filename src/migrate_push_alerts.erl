e4e8579e511d1e8997b2d2850257067cdc611dcb
Murali Balusu <58709405+balusu-murali@users.noreply.github.com>
2021-12-02 17:04:58 -0800

    Enable sending marketing alerts to users in GB and ZA (#2836)
    * Enable sending marketing alerts to users in GB and ZA
    
    - lots of repetitive code: i will clean this up tomorrow and have a more cleaner module to do this.
    - we will need to tag these users: it'll be three countries: i created an issue on this: will do it this week.
    - log last seen

--- src/migrate_push_alerts.erl
+++ src/migrate_push_alerts.erl
@@ -11,13 +11,21 @@
 -include("contacts.hrl").
 -include("account.hrl").
 
--define(MIN_BUCKET, 40).
--define(MAX_BUCKET, 100).
+-define(NG, <<"NG">>).
+-define(GB, <<"GB">>).
+-define(ZA, <<"ZA">>).
+
+-define(MIN_BUCKET, 0).
+-define(MAX_BUCKET, 10).
 
 -export([
     trigger_marketing_alert/2,
     trigger_marketing_alert_nigeria/2,
-    log_account_info_run_nigeria/2
+    log_account_info_run_nigeria/2,
+    trigger_marketing_alert_za/2,
+    log_account_info_run_za/2,
+    trigger_marketing_alert_gb/2,
+    log_account_info_run_gb/2
 ]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -47,9 +55,11 @@ trigger_marketing_alert(Key, State) ->
     end,
     State.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                                  Nigeria (NG)                                      %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 trigger_marketing_alert_nigeria(Key, State) ->
-    % ?INFO("Key: ~p", [Key]),
     DryRun = maps:get(dry_run, State, true),
     Result = re:run(Key, "^acc:{([0-9]+)}$", [global, {capture, all, binary}]),
     case Result of
@@ -60,7 +70,7 @@ trigger_marketing_alert_nigeria(Key, State) ->
                 undefined -> true;
                 _ -> mod_translate:is_langid_english(PushLangId)
             end,
-            case mod_libphonenumber:get_region_id(Phone) =:= <<"NG">> of
+            case mod_libphonenumber:get_region_id(Phone) =:= ?NG of
                 true ->
                     ?INFO("Account uid: ~p, LangId: ~p, ClientVersion: ~p, UidBucket: ~p",
                         [Uid, PushLangId, ClientVersion, UidBucket]),
@@ -85,18 +95,12 @@ trigger_marketing_alert_nigeria(Key, State) ->
     State.
 
 
--spec hash_uid(Uid :: binary()) -> integer().
-hash_uid(Uid) ->
-    crc16_redis:crc16(util:to_list(Uid)) rem 100.
-
-
 log_account_info_run_nigeria(Key, State) ->
-    ?INFO("Key: ~p", [Key]),
     Result = re:run(Key, "^acc:{([0-9]+)}$", [global, {capture, all, binary}]),
     case Result of
         {match, [[FullKey, Uid]]} ->
             {ok, Phone} = q(ecredis_accounts, ["HGET", FullKey, <<"ph">>]),
-            case mod_libphonenumber:get_region_id(Phone) =:= <<"NG">> of
+            case mod_libphonenumber:get_region_id(Phone) =:= ?NG of
                 true ->
                     NumContacts = model_contacts:count_contacts(Uid),
                     {ok, Friends} = model_friends:get_friends(Uid),
@@ -122,5 +126,155 @@ log_account_info_run_nigeria(Key, State) ->
     end,
     State.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                                  South Africa (ZA)                                 %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+trigger_marketing_alert_za(Key, State) ->
+    DryRun = maps:get(dry_run, State, true),
+    Result = re:run(Key, "^acc:{([0-9]+)}$", [global, {capture, all, binary}]),
+    case Result of
+        {match, [[FullKey, Uid]]} ->
+            {ok, [Phone, PushLangId, ClientVersion]} = q(ecredis_accounts, ["HMGET", FullKey, <<"ph">>, <<"pl">>, <<"cv">>]),
+            UidBucket = hash_uid(Uid),
+            IsLangEnglish = case PushLangId of
+                undefined -> true;
+                _ -> mod_translate:is_langid_english(PushLangId)
+            end,
+            case mod_libphonenumber:get_region_id(Phone) =:= ?ZA of
+                true ->
+                    ?INFO("Account uid: ~p, LangId: ~p, ClientVersion: ~p, UidBucket: ~p",
+                        [Uid, PushLangId, ClientVersion, UidBucket]),
+                    case IsLangEnglish andalso UidBucket >= ?MIN_BUCKET andalso UidBucket < ?MAX_BUCKET of
+                        true ->
+                            case DryRun of
+                                true ->
+                                    ?INFO("would send marketing alert to Uid: ~p, LangId: ~p",
+                                        [Uid, PushLangId]);
+                                false ->
+                                    ok = mod_push_notifications:push_marketing_alert(Uid),
+                                    ?INFO("sent marketing alert to: ~p", [Uid])
+                            end;
+                        false ->
+                            ok
+                    end;
+                false ->
+                    ok
+            end;
+        _ -> ok
+    end,
+    State.
+
+
+log_account_info_run_za(Key, State) ->
+    Result = re:run(Key, "^acc:{([0-9]+)}$", [global, {capture, all, binary}]),
+    case Result of
+        {match, [[FullKey, Uid]]} ->
+            {ok, Phone} = q(ecredis_accounts, ["HGET", FullKey, <<"ph">>]),
+            case mod_libphonenumber:get_region_id(Phone) =:= ?ZA of
+                true ->
+                    NumContacts = model_contacts:count_contacts(Uid),
+                    {ok, Friends} = model_friends:get_friends(Uid),
+                    {ok, Contacts} = model_contacts:get_contacts(Uid),
+                    IsSelfContact = model_contacts:is_contact(Uid, Phone),
+                    UidContacts = model_phone:get_uids(Contacts),
+                    case IsSelfContact of
+                        true -> NumUidContacts = length(maps:to_list(UidContacts)) - 1;
+                        false -> NumUidContacts = length(maps:to_list(UidContacts))
+                    end,
+                    IsSelfFriends = model_friends:is_friend(Uid, Uid),
+                    case IsSelfFriends of
+                        true -> NumFriends = length(Friends) - 1;
+                        false -> NumFriends = length(Friends)
+                    end,
+                    CC = mod_libphonenumber:get_cc(Phone),
+                    {ok, Activity} = model_accounts:get_last_activity(Uid),
+                    LastSeenTsMs = Activity#activity.last_activity_ts_ms,
+                    ?INFO("Account Uid: ~p, Phone: ~p, CC: ~p, NumContacts: ~p, NumUidContacts: ~p, NumFriends: ~p, LastSeenTsMs: ~p",
+                        [Uid, Phone, CC, NumContacts, NumUidContacts, NumFriends, LastSeenTsMs]);
+                false ->
+                    ok
+            end;
+        _ -> ok
+    end,
+    State.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                           Great Britain (GB)                                       %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+trigger_marketing_alert_gb(Key, State) ->
+    DryRun = maps:get(dry_run, State, true),
+    Result = re:run(Key, "^acc:{([0-9]+)}$", [global, {capture, all, binary}]),
+    case Result of
+        {match, [[FullKey, Uid]]} ->
+            {ok, [Phone, PushLangId, ClientVersion]} = q(ecredis_accounts, ["HMGET", FullKey, <<"ph">>, <<"pl">>, <<"cv">>]),
+            UidBucket = hash_uid(Uid),
+            IsLangEnglish = case PushLangId of
+                undefined -> true;
+                _ -> mod_translate:is_langid_english(PushLangId)
+            end,
+            case mod_libphonenumber:get_region_id(Phone) =:= ?GB of
+                true ->
+                    ?INFO("Account uid: ~p, LangId: ~p, ClientVersion: ~p, UidBucket: ~p",
+                        [Uid, PushLangId, ClientVersion, UidBucket]),
+                    case IsLangEnglish andalso UidBucket >= ?MIN_BUCKET andalso UidBucket < ?MAX_BUCKET of
+                        true ->
+                            case DryRun of
+                                true ->
+                                    ?INFO("would send marketing alert to Uid: ~p, LangId: ~p",
+                                        [Uid, PushLangId]);
+                                false ->
+                                    ok = mod_push_notifications:push_marketing_alert(Uid),
+                                    ?INFO("sent marketing alert to: ~p", [Uid])
+                            end;
+                        false ->
+                            ok
+                    end;
+                false ->
+                    ok
+            end;
+        _ -> ok
+    end,
+    State.
+
+
+log_account_info_run_gb(Key, State) ->
+    Result = re:run(Key, "^acc:{([0-9]+)}$", [global, {capture, all, binary}]),
+    case Result of
+        {match, [[FullKey, Uid]]} ->
+            {ok, Phone} = q(ecredis_accounts, ["HGET", FullKey, <<"ph">>]),
+            case mod_libphonenumber:get_region_id(Phone) =:= ?GB of
+                true ->
+                    NumContacts = model_contacts:count_contacts(Uid),
+                    {ok, Friends} = model_friends:get_friends(Uid),
+                    {ok, Contacts} = model_contacts:get_contacts(Uid),
+                    IsSelfContact = model_contacts:is_contact(Uid, Phone),
+                    UidContacts = model_phone:get_uids(Contacts),
+                    case IsSelfContact of
+                        true -> NumUidContacts = length(maps:to_list(UidContacts)) - 1;
+                        false -> NumUidContacts = length(maps:to_list(UidContacts))
+                    end,
+                    IsSelfFriends = model_friends:is_friend(Uid, Uid),
+                    case IsSelfFriends of
+                        true -> NumFriends = length(Friends) - 1;
+                        false -> NumFriends = length(Friends)
+                    end,
+                    CC = mod_libphonenumber:get_cc(Phone),
+                    {ok, Activity} = model_accounts:get_last_activity(Uid),
+                    LastSeenTsMs = Activity#activity.last_activity_ts_ms,
+                    ?INFO("Account Uid: ~p, Phone: ~p, CC: ~p, NumContacts: ~p, NumUidContacts: ~p, NumFriends: ~p, LastSeenTsMs: ~p",
+                        [Uid, Phone, CC, NumContacts, NumUidContacts, NumFriends, LastSeenTsMs]);
+                false ->
+                    ok
+            end;
+        _ -> ok
+    end,
+    State.
+
+
+-spec hash_uid(Uid :: binary()) -> integer().
+hash_uid(Uid) ->
+    crc16_redis:crc16(util:to_list(Uid)) rem 100.
 
 q(Client, Command) -> util_redis:q(Client, Command).
