8a87fce05864f05fcf27c4c2762f7708edee0199
Vipin Gupta <vipin@halloapp.com>
2022-01-06 11:31:58 -0800

    Increase time from 185 days to 245 days for inactive users

--- include/mod_inactive_accounts.hrl
+++ include/mod_inactive_accounts.hrl
@@ -8,8 +8,8 @@
 -ifndef(MOD_INACTIVE_ACCOUNTS_HRL).
 -define(MOD_INACTIVE_ACCOUNTS_HRL, 1).
 
-%% Designate account inactive if last activity 185 days (approximately 6 months) ago.
--define(NUM_INACTIVITY_DAYS, 185).
+%% Designate account inactive if last activity 245 days (approximately 8 months) ago.
+-define(NUM_INACTIVITY_DAYS, 245).
 
 %% We are ok with deletion of 1% of inactive accounts every week.
 -define(ACCEPTABLE_FRACTION, 0.018).
