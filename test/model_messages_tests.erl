b0f06825151649cae53cc2308cc21176abc8c5e0
balusu-murali <murali@halloapp.com>
2022-02-03 18:46:34 -0800

    Fix ct tests
    - fix ci tests failing.

--- test/model_messages_tests.erl
+++ test/model_messages_tests.erl
@@ -132,36 +132,36 @@ max_num_offline_messages_test() ->
     setup(),
     ?assertEqual(ok, model_messages:remove_all_user_messages(?UID1)),
     ?assertEqual({ok, 0}, model_messages:count_user_messages(?UID1)),
-    N = 100,
+    N = 200,
     util:rev_while(N,
         fun(X) ->
             Xbin = util:to_binary(X),
             MsgId = <<?MID1/binary, "-", Xbin/binary>>,
             ?assertEqual(ok, model_messages:store_message(?UID1, undefined, MsgId, ?TYPE1, ?GROUP1, ?MESSAGE1))
         end),
-    ?assertEqual({ok, 100}, model_messages:count_user_messages(?UID1)),
+    ?assertEqual({ok, 200}, model_messages:count_user_messages(?UID1)),
 
     %% Insert 101th message.
-    MsgId2 = <<?MID1/binary, "-101">>,
+    MsgId2 = <<?MID1/binary, "-201">>,
     %% This should be overflow??
     ?assertEqual({ok, overflow}, model_messages:store_message(?UID1, undefined, MsgId2, ?TYPE1, ?GROUP1, ?MESSAGE1)),
-    ?assertEqual({ok, 100}, model_messages:count_user_messages(?UID1)),
+    ?assertEqual({ok, 200}, model_messages:count_user_messages(?UID1)),
     ?assertEqual({ok, true}, model_messages:is_queue_trimmed(?UID1)),
 
     %% Count should still be 100.
-    ?assertEqual({ok, 100}, model_messages:count_user_messages(?UID1)),
+    ?assertEqual({ok, 200}, model_messages:count_user_messages(?UID1)),
 
     %% Ack the missing message.
-    MsgId3 = <<?MID1/binary, <<"-100">>/binary>>,
+    MsgId3 = <<?MID1/binary, <<"-200">>/binary>>,
     ?assertEqual(ok, model_messages:ack_message(?UID1, MsgId3)),
     %% Count should still be 100.
-    ?assertEqual({ok, 100}, model_messages:count_user_messages(?UID1)),
+    ?assertEqual({ok, 200}, model_messages:count_user_messages(?UID1)),
 
     %% Ack message 99.
-    MsgId4 = <<?MID1/binary, <<"-99">>/binary>>,
+    MsgId4 = <<?MID1/binary, <<"-199">>/binary>>,
     ?assertEqual(ok, model_messages:ack_message(?UID1, MsgId4)),
     %% Count should now be 99.
-    ?assertEqual({ok, 99}, model_messages:count_user_messages(?UID1)),
+    ?assertEqual({ok, 199}, model_messages:count_user_messages(?UID1)),
     ok.
 
 
