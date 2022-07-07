1b81057ad0da54a73a4db14b3263599a8dd76079
Chris Leonavicius <chris@halloapp.com>
2022-07-07 10:24:41 -0700

    Add server prop for enabling sentry performance tracking

--- src/mod_props.erl
+++ src/mod_props.erl
@@ -131,7 +131,8 @@ get_props(Uid, ClientVersion) ->
         invite_strings => get_invite_strings_bin(), %% json string with invite text.
         new_chat_ui => false,   %% turn on new chat ui on ios.
         is_psa_admin => false, %% is client allowed to post PSA Moment
-        default_krisp_noise_suppression => false  %% Should client use noise suppression by default
+        default_krisp_noise_suppression => false,  %% Should client use noise suppression by default
+        enable_sentry_perf_tracking => false %% Enable Sentry perf tracking on iOS clients
     },
     PropMap2 = get_uid_based_props(PropMap1, Uid),
     ClientType = util_ua:get_client_type(ClientVersion),
@@ -159,7 +160,8 @@ get_uid_based_props(PropMap, Uid) ->
             PropMap10 = maps:update(use_cleartext_group_feed, false, PropMap9),
             PropMap11 = maps:update(new_chat_ui, true, PropMap10),
             PropMap12 = maps:update(default_krisp_noise_suppression, true, PropMap11),
-            PropMap12
+            PropMap13 = maps:update(enable_sentry_perf_tracking, true, PropMap12),
+            PropMap13
     end,
     apply_uid_prop_overrides(Uid, ResPropMap).
 
