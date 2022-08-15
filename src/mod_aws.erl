183baaf276dfdb60ca08c321c7e20077815bea49
balusu-murali <murali@halloapp.com>
2022-08-15 16:01:19 -0700

    Hack to avoid counting noise checkers
    - use specific ip for s-test.

--- src/mod_aws.erl
+++ src/mod_aws.erl
@@ -233,6 +233,7 @@ get_machines_internal() ->
     end.
 
 -spec get_ip(MachineName :: string()) -> string().
+get_ip("s-test") -> "100.27.28.171";
 get_ip(MachineName) ->
     case lists:keyfind(MachineName, 1, get_ejabberd_machines()) of
         false -> "";
