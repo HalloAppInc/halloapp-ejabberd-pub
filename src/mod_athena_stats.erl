5836a7ca2f107e3b126495f2b13f4a109ca3fcc0
kennedyjosh <32585114+kennedyjosh@users.noreply.github.com>
2022-12-22 15:25:47 -0800

    Remove athena_call (#3777)

--- src/mod_athena_stats.erl
+++ src/mod_athena_stats.erl
@@ -89,10 +89,6 @@ handle_cast(run_athena_queries, State) ->
     Queries = get_athena_queries(),
     State2 = run_queries_internal(Queries, State),
     {noreply, State2};
-handle_cast(run_hourly_athena_queries, State) ->
-    Queries = get_hourly_athena_queries(),
-    State2 = run_queries_internal(Queries, State),
-    {noreply, State2};
 handle_cast({fetch_query_results, ExecutionId}, State) ->
     {noreply, fetch_query_results_internal(ExecutionId, State)};
 handle_cast({delete_query, Id}, #{queries := Queries} = State) ->
@@ -122,7 +118,6 @@ run_athena_queries() ->
             end;
         _ -> ok
     end,
-    gen_server:cast(?PROC(), run_hourly_athena_queries),
     ok.
 
 run_query(Query) ->
@@ -294,14 +289,6 @@ get_athena_queries() ->
         end, Modules),
     lists:flatten(Queries).
 
-get_hourly_athena_queries() ->
-    Modules = get_hourly_athena_modules(),
-    Queries = lists:map(
-        fun(Module) ->
-            erlang:apply(Module, get_hourly_queries, [])
-        end, Modules),
-    lists:flatten(Queries).
-
 
 get_athena_modules() ->
     [
@@ -309,11 +296,6 @@ get_athena_modules() ->
         athena_group_encryption,
         athena_home_encryption,
         athena_push,
-        athena_push_api,
-        athena_call
+        athena_push_api
     ].
 
-get_hourly_athena_modules() ->
-    [
-        athena_call
-    ].
