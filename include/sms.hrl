9e81015b0e15c44dcfbfec0ab15f473506d6caf1
Murali Balusu <58709405+balusu-murali@users.noreply.github.com>
2023-02-28 17:19:01 -0800

    Update android sms autofill hash. (#3996)

--- include/sms.hrl
+++ include/sms.hrl
@@ -16,7 +16,7 @@
 
 -define(ANDROID_DEBUG_HASH, <<"/TwOjtaTNFA">>).
 -define(ANDROID_RELEASE_HASH, <<"05IyxZs5b3I">>).
--define(KATCHUP_ANDROID_RELEASE_HASH, <<"dsBIR3o/cGT">>).
+-define(KATCHUP_ANDROID_RELEASE_HASH, <<"6IOW7RE1Oz9">>).
 -define(KATCHUP_ANDROID_DEBUG_HASH, <<"pYKP1cEfSmq">>).
 
 -define(TWILIO, <<"twilio">>).
