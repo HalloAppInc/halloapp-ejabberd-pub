92946672096bf15c956d5437367da2088c102750
Murali Balusu <58709405+balusu-murali@users.noreply.github.com>
2023-03-02 10:24:03 -0800

    Reset index of seen posts properly (#4002)
    - made it a zset now to cleanup old posts and based the key off only uid.
    - the problem is with a notif-id you see posts from a different notif-id and that messes up public feed ranking at times.
    - this diff fixes it by making it based off uid and a zset.
    - but we dont want to pile up this zset forever: so we clean it up reads.
    - old keys will expire shortly - so nothing to clean up.

--- src/models/model_feed.erl
+++ src/models/model_feed.erl
@@ -104,9 +104,7 @@
     get_all_posts_by_time_bucket/3,
     get_all_posts_by_geo_tag_time_bucket/4,
     mark_seen_posts/2,
-    mark_seen_posts/3,
     get_past_seen_posts/1,
-    get_past_seen_posts/2,
     clear_past_seen_posts/1,
     get_moment_info/1,
     get_post_num_seen/1,
@@ -313,52 +311,27 @@ parse_score_and_explanation([{ok, Score}, {ok, Exp} | Rest], Res) ->
 
 mark_seen_posts(_Uid, []) -> ok;
 mark_seen_posts(Uid, PostIds) when is_list(PostIds) ->
-    case get_notification_id(Uid) of
-        undefined ->
-            %% this should never happen.
-            ?ERROR("Uid: ~p undefined notification_id", [Uid]),
-            ok;
-        LatestNotifId ->
-            mark_seen_posts(Uid, LatestNotifId, PostIds),
-            ok
-    end,
+    TimestampMs = util:now_ms(),
+    PostIdsWithScores = lists:flatmap(
+        fun(PostId) -> [TimestampMs, PostId] end, PostIds),
+    [{ok, _}, {ok, _}] = qp([
+        ["ZADD", seen_posts_key(Uid)] ++ PostIdsWithScores,
+        ["EXPIRE", seen_posts_key(Uid), ?MOMENT_TAG_EXPIRATION]]),
     ok;
 mark_seen_posts(Uid, PostId) ->
     mark_seen_posts(Uid, [PostId]).
 
 
-mark_seen_posts(_Uid, _LatestNotifId, []) -> ok;
-mark_seen_posts(Uid, LatestNotifId, PostIds) ->
-    [{ok, _}, {ok, _}] = qp([
-                    ["SADD", seen_posts_key(Uid, LatestNotifId)] ++ PostIds,
-                    ["EXPIRE", seen_posts_key(Uid, LatestNotifId), ?MOMENT_TAG_EXPIRATION]]),
-    ok.
-
-
 get_past_seen_posts(Uid) ->
-    case get_notification_id(Uid) of
-        undefined ->
-            %% this should never happen.
-            ?ERROR("Uid: ~p undefined notification_id", [Uid]),
-            [];
-        LatestNotifId ->
-            get_past_seen_posts(Uid, LatestNotifId)
-    end.
-
-
-get_past_seen_posts(Uid, LatestNotifId) ->
-    {ok, PostIds} = q(["SMEMBERS", seen_posts_key(Uid, LatestNotifId)]),
+    DeadlineMs = util:now_ms() - ?KATCHUP_MOMENT_EXPIRATION_MS,
+    [{ok, PostIds}, {ok, _}] = qp([
+        ["ZRANGE", seen_posts_key(Uid), util:to_binary(DeadlineMs), "+inf", "BYSCORE"],
+        ["ZREMRANGE", seen_posts_key(Uid), "-inf", util:to_binary(DeadlineMs), "BYSCORE"]]),
     PostIds.
 
 
 clear_past_seen_posts(Uid) ->
-    case get_notification_id(Uid) of
-        undefined ->
-            %% this should never happen.
-            ?ERROR("Uid: ~p undefined notification_id", [Uid]);
-        LatestNotifId ->
-            q(["DEL", seen_posts_key(Uid, LatestNotifId)])
-    end,
+    q(["DEL", seen_posts_key(Uid)]),
     ok.
 
 
@@ -1571,10 +1544,9 @@ geo_tag_time_bucket_key_hr(GeoTag, TimestampHr) ->
     <<?GEO_TAG_TIME_BUCKET_KEY/binary, ":", GeoTagBin/binary, "{", TimestampHrBin/binary, "}">>.
 
 
--spec seen_posts_key(Uid :: binary(), LatestNotifId :: integer()) -> binary().
-seen_posts_key(Uid, LatestNotifId) ->
-    LatestNotifIdBin = util:to_binary(LatestNotifId),
-    <<?SEEN_POSTS_KEY/binary, "{", Uid/binary, "}:", LatestNotifIdBin/binary>>.
+-spec seen_posts_key(Uid :: binary()) -> binary().
+seen_posts_key(Uid) ->
+    <<?SEEN_POSTS_KEY/binary, "{", Uid/binary, "}">>.
 
 
 num_seen_key(PostId) ->
