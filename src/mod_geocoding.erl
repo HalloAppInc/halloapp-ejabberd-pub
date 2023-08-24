87c02f95aa3036fefb0249fbe405176075f43d1d
kennedyjosh <32585114+kennedyjosh@users.noreply.github.com>
2023-08-24 15:55:09 -0700

    Make geocoding proxy do async requests (#4165)

--- src/mod_geocoding.erl
+++ src/mod_geocoding.erl
@@ -9,14 +9,18 @@
 -author("josh").
 
 -behavior(gen_mod).
+-behavior(gen_server).
 
 -include("ha_types.hrl").
 -include("logger.hrl").
 -include("proc.hrl").
 -include("server.hrl").
 
-%% gen_mod API.
+%% gen_mod API
 -export([start/2, stop/1, reload/3, depends/2, mod_options/1]).
+
+%% gen_server API
+-export([init/1, terminate/2, handle_cast/2, handle_call/3, handle_info/2, code_change/3]).
 
 %% API
 -export([
@@ -36,46 +40,8 @@ process_local_iq(#pb_iq{payload = #pb_reverse_geocode_request{location = #pb_gps
 process_local_iq(#pb_iq{from_uid = Uid, payload = #pb_reverse_geocode_request{location = #pb_gps_location{
         latitude = Latitude, longitude = Longitude}}} = IQ) ->
     ?INFO("~s requesting reverse geocode lookup for ~f, ~f", [Uid, Latitude, Longitude]),
-    BinLat = float_to_binary(Latitude),
-    BinLong = float_to_binary(Longitude),
-    AccessToken = mod_aws:get_secret(<<"Mapbox">>),
-    Url = <<"https://api.mapbox.com/geocoding/v5/mapbox.places/", BinLong/binary, ",", BinLat/binary, ".json?",
-        "types=poi,address&reverseMode=score&access_token=", AccessToken/binary>>,
-    case httpc:request(Url) of
-        {ok, {{_, 200, _}, _Headers, JsonResult}} ->
-            ResultMap = jiffy:decode(JsonResult, [return_maps]),
-            case maps:get(<<"features">>, ResultMap) of
-                [] ->
-                    pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = ok});
-                FeaturesList ->
-                    FeatureTypes = lists:map(fun(Map) -> lists:nth(1, binary:split(maps:get(<<"id">>, Map, undefined), <<".">>)) end, FeaturesList),
-                    case {lists:member(<<"poi">>, FeatureTypes), lists:member(<<"address">>, FeatureTypes)} of
-                        {true, _} ->
-                            %% poi exists
-                            Feature = get_feature(<<"poi">>, FeaturesList),
-                            Name = maps:get(<<"text">>, Feature, <<>>),
-                            Address = maps:get(<<"address">>, maps:get(<<"properties">>, Feature, #{}), <<>>),
-                            ReverseGeocodeLocation = get_location_info_from_context(Feature,
-                                #pb_reverse_geocode_location{name = Name, address = Address, type = <<"poi">>}),
-                            pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = ok, location = ReverseGeocodeLocation});
-                        {_, true} ->
-                            %% address exists
-                            Feature = get_feature(<<"address">>, FeaturesList),
-                            Street = maps:get(<<"text">>, Feature, <<>>),
-                            Number = maps:get(<<"address">>, Feature, <<"">>),
-                            ReverseGeocodeLocation = get_location_info_from_context(Feature,
-                                #pb_reverse_geocode_location{address = string:trim(<<Number/binary, " ", Street/binary>>), type = <<"address">>}),
-                            pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = ok, location = ReverseGeocodeLocation});
-                        _ ->
-                            %% no poi or address info
-                            ?INFO("No poi or address for ~s's request: (~f, ~f)", [Uid, Latitude, Longitude]),
-                            pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = fail})
-                    end
-            end;
-        Err ->
-            ?ERROR("Error fetching reverse geocode for (~f, ~f): ~p", [Latitude, Longitude, Err]),
-            pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = fail})
-    end.
+    gen_server:cast(?PROC(), {reverse_geocode_lookup, Uid, Latitude, Longitude, IQ, self()}),
+    ignore.
 
 %%====================================================================
 %% gen_mod functions
@@ -83,11 +49,13 @@ process_local_iq(#pb_iq{from_uid = Uid, payload = #pb_reverse_geocode_request{lo
 
 start(Host, Opts) ->
     ?INFO("Start: ~w", [?MODULE]),
+    gen_mod:start_child(?MODULE, Host, Opts, ?PROC()),
     gen_iq_handler:add_iq_handler(ejabberd_local, ?HALLOAPP, pb_reverse_geocode_request, ?MODULE, process_local_iq),
     ok.
 
 stop(_Host) ->
     ?INFO("Stop: ~w", [?MODULE]),
+    gen_mod:stop_child(?PROC()),
     gen_iq_handler:remove_iq_handler(ejabberd_local, ?HALLOAPP, pb_reverse_geocode_request),
     ok.
 
@@ -99,6 +67,111 @@ reload(_Host, _NewOpts, _OldOpts) ->
 
 mod_options(_Host) ->
     [].
+
+%%====================================================================
+%% gen_server API
+%%====================================================================
+
+init(_) ->
+    AccessToken = mod_aws:get_secret(<<"Mapbox">>),
+    {ok, #{access_token => AccessToken, ref_map => #{}}}.
+
+
+terminate(_Reason, _State) ->
+    ok.
+
+
+handle_call(Msg, From, State) ->
+    ?WARNING("Unexpected call from ~p: ~p", [From, Msg]),
+    {noreply, State}.
+
+
+handle_cast({reverse_geocode_lookup, Uid, Latitude, Longitude, IQ, C2SPid},
+        #{access_token := AccessToken, ref_map := RefMap} = State) ->
+    BinLat = float_to_binary(Latitude),
+    BinLong = float_to_binary(Longitude),
+    Url = <<"https://api.mapbox.com/geocoding/v5/mapbox.places/", BinLong/binary, ",", BinLat/binary, ".json?",
+        "types=poi,address&reverseMode=score&access_token=", AccessToken/binary>>,
+    NewState = case httpc:request(get, {Url, []}, [], [{sync, false}]) of
+        {ok, Ref} ->
+            NewRefMap = RefMap#{Ref => {Uid, IQ, C2SPid}},
+            State#{ref_map => NewRefMap};
+        {error, Err} ->
+            ?ERROR("httpc error for url ~p: ~p", [Url, Err]),
+            IQResult = pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = fail}),
+            halloapp_c2s:route(C2SPid, {route, IQResult}),
+            State
+    end,
+    {noreply, NewState};
+
+handle_cast({ping, Id, Ts, From}, State) ->
+    util_monitor:send_ack(self(), From, {ack, Id, Ts, self()}),
+    {noreply, State};
+
+handle_cast(Msg, State) ->
+    ?WARNING("Unexpected cast: ~p", [Msg]),
+    {noreply, State}.
+
+
+%% handles return message from async httpc request
+handle_info({http, {Ref, {{_, 200, "OK"}, _Hdrs, JsonResult}}}, #{ref_map := RefMap} = State) ->
+    ResultMap = jiffy:decode(JsonResult, [return_maps]),
+    {Uid, IQ, C2SPid} = maps:get(Ref, RefMap),
+    IQResult = case maps:get(<<"features">>, ResultMap) of
+        [] ->
+            pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = ok});
+        FeaturesList ->
+            FeatureTypes = lists:map(fun(Map) -> lists:nth(1, binary:split(maps:get(<<"id">>, Map, undefined), <<".">>)) end, FeaturesList),
+            case {lists:member(<<"poi">>, FeatureTypes), lists:member(<<"address">>, FeatureTypes)} of
+                {true, _} ->
+                    %% poi exists
+                    Feature = get_feature(<<"poi">>, FeaturesList),
+                    Name = maps:get(<<"text">>, Feature, <<>>),
+                    Address = maps:get(<<"address">>, maps:get(<<"properties">>, Feature, #{}), <<>>),
+                    ReverseGeocodeLocation = get_location_info_from_context(Feature,
+                        #pb_reverse_geocode_location{name = Name, address = Address, type = <<"poi">>}),
+                    pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = ok, location = ReverseGeocodeLocation});
+                {_, true} ->
+                    %% address exists
+                    Feature = get_feature(<<"address">>, FeaturesList),
+                    Street = maps:get(<<"text">>, Feature, <<>>),
+                    Number = maps:get(<<"address">>, Feature, <<"">>),
+                    ReverseGeocodeLocation = get_location_info_from_context(Feature,
+                        #pb_reverse_geocode_location{address = string:trim(<<Number/binary, " ", Street/binary>>), type = <<"address">>}),
+                    pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = ok, location = ReverseGeocodeLocation});
+                _ ->
+                    %% no poi or address info
+                    ?INFO("No poi or address for ~s's request", [Uid]),
+                    pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = ok})
+            end
+    end,
+    halloapp_c2s:route(C2SPid, {route, IQResult}),
+    NewRefMap = maps:remove(Ref, RefMap),
+    {noreply, State#{ref_map => NewRefMap}};
+
+handle_info({http, {Ref, {{_, Code, HttpMsg}, _Hdrs, _Res}}}, #{ref_map := RefMap} = State) ->
+    {_Uid, IQ, C2SPid} = maps:get(Ref, RefMap),
+    ?ERROR("~p error: ~p | initial request: ~p", [Code, HttpMsg, IQ]),
+    IQResult = pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = fail}),
+    halloapp_c2s:route(C2SPid, {route, IQResult}),
+    NewRefMap = maps:remove(Ref, RefMap),
+    {noreply, State#{ref_map => NewRefMap}};
+
+handle_info({http, {Ref, {error, Reason}}}, #{ref_map := RefMap} = State) ->
+    {_Uid, IQ, C2SPid} = maps:get(Ref, RefMap),
+    ?ERROR("Error: ~p | initial request: ~p", [Reason, IQ]),
+    IQResult = pb:make_iq_result(IQ, #pb_reverse_geocode_result{result = fail}),
+    halloapp_c2s:route(C2SPid, {route, IQResult}),
+    NewRefMap = maps:remove(Ref, RefMap),
+    {noreply, State#{ref_map => NewRefMap}};
+
+handle_info(Info, State) ->
+    ?WARNING("Unexpected info: ~p", [Info]),
+    {noreply, State}.
+
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %%====================================================================
 %% Internal functions
