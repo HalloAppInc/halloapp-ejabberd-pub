976f614e505b792f543348730f36db853fb6d637
kennedyjosh <32585114+kennedyjosh@users.noreply.github.com>
2023-11-16 12:25:29 -0600

    Shorten inactive account definition from 6 months to 5 months (#4201)

--- include/mod_inactive_accounts.hrl
+++ include/mod_inactive_accounts.hrl
@@ -8,8 +8,8 @@
 -ifndef(MOD_INACTIVE_ACCOUNTS_HRL).
 -define(MOD_INACTIVE_ACCOUNTS_HRL, 1).
 
-%% Designate account inactive if last activity 185 days (approximately 6 months) ago.
--define(NUM_INACTIVITY_DAYS, 185).
+%% Designate account inactive if last activity 185 days (approximately 5 months) ago.
+-define(NUM_INACTIVITY_DAYS, 150).
 
 %% We are ok with deletion of 1% of inactive accounts every week.
 -define(ACCEPTABLE_FRACTION, 0.018).
