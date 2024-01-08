cbb72f3cad8a81a69f19249a30dbbdeedf18de93
kennedyjosh <32585114+kennedyjosh@users.noreply.github.com>
2024-01-08 13:18:32 -0600

    Block all Russian SMS attempts (#4212)

--- src/otp_checker_ip_geo.erl
+++ src/otp_checker_ip_geo.erl
@@ -28,9 +28,11 @@ check_otp_request(Phone, IP, _UserAgent, _Method, _Protocol, _RemoteStaticKey) -
     IPCC = mod_geodb:lookup(IP),
     check_otp_request(Phone, IP, PhoneCC, IPCC).
 
-% Block AZ in all cases.
+% Block AZ & RU in all cases.
 check_otp_request(_Phone, _IP, <<"AZ">>, CC) ->
     {block, phone_cc_block, {<<"AZ">>, CC}};
+check_otp_request(_Phone, _IP, <<"RU">>, CC) ->
+    {block, phone_cc_block, {<<"RU">>, CC}};
 %% Allow in case IP of Phone and CC is same.
 check_otp_request(_Phone, _IP, PhoneCC, PhoneCC) ->
     ok;
@@ -54,8 +56,6 @@ check_otp_request(_Phone, _IP, <<"PH">>, CC) ->
     {block, phone_cc_block, {<<"PH">>, CC}};
 check_otp_request(_Phone, _IP, <<"UZ">>, CC) ->
     {block, phone_cc_block, {<<"UZ">>, CC}};
-check_otp_request(_Phone, _IP, <<"RU">>, CC) ->
-    {block, phone_cc_block, {<<"RU">>, CC}};
 check_otp_request(_PHONE, IP, _PhoneCC, <<"ZZ">>) ->
     ?WARNING("Unable to find cc for ip: ~s", [IP]),
     ok;
